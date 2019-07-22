7c325dd8dc4ea51ad25ef235f6bcff44ad928287
virdiignis <gbryk1@gmail.com>
2019-07-22 12:39:20 +0200

    login page

--- src/apps/core/urls.py
+++ src/apps/core/urls.py
@@ -4,4 +4,5 @@ from src.apps.core import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
 ]
--- src/apps/core/views.py
+++ src/apps/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from django.http import HttpResponse, HttpRequest
 
@@ -48,3 +48,28 @@ def signup(request):  # type: (HttpRequest) -> HttpResponse
     }
 
     return render(request, 'core/signup.html', context)
+
+
+def login(request):  # type: (HttpRequest) -> HttpResponse
+    if request.method == 'POST':
+        # form = SignUpForm(request.POST)
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+
+    context = {
+        'title': 'Home',
+        'alerts': None,
+        'form': form,
+    }
+
+    return render(request, 'core/login.html', context)
