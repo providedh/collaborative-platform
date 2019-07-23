5f12228fd07fce6c53ccbf0cd52b00f67790a309
virdiignis <gbryk1@gmail.com>
2019-07-23 13:32:02 +0200

    database backend change

--- src/collaborative_platform/collaborative_platform/settings.py
+++ src/collaborative_platform/collaborative_platform/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'collaborative_platform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'collaborative_platform',
+        'USER': 'collaborative_platform',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
