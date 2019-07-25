90f1b4c2473a94acc14154d230e68b887140f046
bug-rancher <bug-rancher@protonmail.com>
2019-07-25 14:46:29 +0200

    Add view for project

--- src/collaborative_platform/apps/projects/views.py
+++ src/collaborative_platform/apps/projects/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, redirect
+from .models import Project, Contributor
 
 
 @login_required()
-def projects(request, user_id):
+def projects(request, user_id):  # type: (HttpRequest, int) -> HttpResponse
     if user_id == request.user.pk:
         context = {
             'title': 'Projects',
@@ -22,8 +23,53 @@ def projects(request, user_id):
         ]
 
         context = {
-            'title': 'Projects',
+            'title': 'Home',
             'alerts': alerts,
         }
 
         return render(request, 'core/index.html', context)
+
+
+@login_required()
+def project(request, project_id):  # type: (HttpRequest, int) -> HttpResponse
+    alerts = []
+
+    try:
+        project = Project.objects.get(id=project_id)
+    except Project.DoesNotExist:
+        alert = {
+            'type': 'warning',
+            'message': "Project with id: {} doesn't exist.".format(project_id)
+        }
+
+        alerts.append(alert)
+
+        context = {
+            'title': 'Home',
+            'alerts': alerts,
+        }
+
+        return render(request, 'core/index.html', context)
+
+    contributor = Contributor.objects.filter(project_id=project_id, user_id=request.user.pk)
+    if not contributor:
+        alert = {
+            'type': 'warning',
+            'message': "You aren't contributor in project with id: {}.".format(project_id)
+        }
+
+        alerts.append(alert)
+
+        context = {
+            'title': 'Home',
+            'alerts': alerts,
+        }
+
+        return render(request, 'core/index.html', context)
+
+    context = {
+        'title': project.title,
+        'alerts': None,
+    }
+
+    return render(request, 'projects/project.html', context)
