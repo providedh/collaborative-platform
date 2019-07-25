09259d2f67a6d37c252d414f214c2bf0e2399c66
virdiignis <gbryk1@gmail.com>
2019-07-25 09:41:36 +0200

    upload return codes

--- src/collaborative_platform/apps/files_management/views.py
+++ src/collaborative_platform/apps/files_management/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.http import HttpRequest, HttpResponse, HttpResponseServerError, HttpResponseBadRequest
 from django.shortcuts import render
 from .files_management import upload_file
 
@@ -6,7 +6,12 @@ from .files_management import upload_file
 def upload(request):  # type: (HttpRequest) -> HttpResponse
     if request.method == "POST" and request.FILES:
         files_list = request.FILES.getlist("files")
-        for file in files_list:
-            upload_file(file)
-        return HttpResponseRedirect("/")  # TODO: replace this with actual path
-    raise
+        if not files_list:
+            return HttpResponseBadRequest("File not attached properly")
+        try:
+            for file in files_list:
+                upload_file(file)
+        except:
+            return HttpResponseServerError("Unknown error while uploading")
+        return HttpResponse("OK")
+    return HttpResponseBadRequest("Invalid request method or files not attached")
