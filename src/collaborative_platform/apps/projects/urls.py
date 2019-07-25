ed098184249c75b23aab212466ef2cafa5110e80
virdiignis <gbryk1@gmail.com>
2019-07-25 07:54:25 +0200

    api for getting public projects after login

--- src/collaborative_platform/apps/projects/urls.py
+++ src/collaborative_platform/apps/projects/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create, name='create'),
+    path('get_public/', views.get_public, name='get_public'),
 ]
--- src/collaborative_platform/apps/projects/views.py
+++ src/collaborative_platform/apps/projects/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ValidationError
-from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest, JsonResponse
 from django.shortcuts import render
 from json import loads, JSONDecodeError
 
@@ -31,3 +31,20 @@ def create(request):  # type: (HttpRequest) -> HttpResponse
             return HttpResponseBadRequest("Invalid value")
 
     return HttpResponseBadRequest("Invalid request type or empty request")
+
+
+@login_required(login_url="/login/")
+def get_public(request):  # type: (HttpRequest) -> HttpResponse
+    if request.method != "GET":
+        return HttpResponseBadRequest("Invalid request method")
+
+    page = request.GET.get("page", 1)
+    per_page = request.GET.get("per_page", 5)
+    per_page = min(per_page, 50)  # limited for safety reasons
+
+    start = (page - 1) * per_page
+    end = start + per_page + 1
+
+    projects = list(Project.objects.filter(public=True)[start:end].values())
+
+    return JsonResponse(projects, safe=False)
