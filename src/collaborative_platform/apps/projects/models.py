ddb7ccba172b2063494160883c1d5c1e1023eb59
virdiignis <gbryk1@gmail.com>
2019-08-07 16:35:48 +0200

    model for activities in project

--- src/collaborative_platform/apps/projects/models.py
+++ src/collaborative_platform/apps/projects/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from apps.files_management.models import FileNode
+
 
 class Project(models.Model):
     title = models.CharField(max_length=150, blank=False)
@@ -25,7 +27,16 @@ class Contributor(models.Model):
 
     project = models.ForeignKey(Project, related_name='contributors', on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name='contributions', on_delete=models.CASCADE)
-    permissions = models.CharField(max_length=2, choices=permissions_levels, default="RO")
+    permissions = models.CharField(max_length=2, limit_choices_to=permissions_levels, default="RO")
 
     class Meta:
         unique_together = ("project", "user")
+
+
+class Activity(models.Model):
+    project = models.ForeignKey(Project, related_name='activities', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    related_filenode = models.ForeignKey(FileNode, related_name='activities', on_delete=models.DO_NOTHING,
+                                         null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='activities')
+    action_text = models.CharField()
