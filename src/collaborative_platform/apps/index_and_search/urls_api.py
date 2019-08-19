55ece8482d8eebf60d96159e2767ad9a9a8ca429
virdiignis <gbryk1@gmail.com>
2019-08-19 11:38:33 +0200

    finished fuzzysearch api

--- src/collaborative_platform/apps/index_and_search/urls_api.py
+++ src/collaborative_platform/apps/index_and_search/urls_api.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import api
 
 urlpatterns = [
-    path('entity_completion/<str:entity_type>/<str:query>/', api.entity_completion, name='entity_completion'),
+    path('entity_completion/project/<int:project_id>/entity/<str:entity_type>/<str:query>', api.entity_completion,
+         name='entity_completion'),
 ]
