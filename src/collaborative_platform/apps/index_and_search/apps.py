2a7da5722e0c2e3cc39fd37bfa9a4bd8d2736398
virdiignis <gbryk1@gmail.com>
2019-08-19 11:39:15 +0200

    finished fuzzysearch api

---
+++ src/collaborative_platform/apps/index_and_search/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IndexAndSearchConfig(AppConfig):
+    name = 'index_and_search'
---
+++ src/collaborative_platform/apps/index_and_search/models.py
@@ -0,0 +1,78 @@
+from itertools import permutations
+
+from elasticsearch_dsl import Document, Integer, Text, Keyword, Completion, analyzer, token_filter, GeoPoint, Date, Long
+
+# custom analyzer for names
+ascii_fold = analyzer(
+    'ascii_fold',
+    # we don't want to split O'Brian or Toulouse-Lautrec
+    tokenizer='whitespace',
+    filter=[
+        'lowercase',
+        token_filter('ascii_fold', 'asciifolding')
+    ]
+)
+
+
+class Entity(Document):
+    project_id = Integer()
+    file_id = Integer()
+    id = Text()
+    name = Text(fields={'keywords': Keyword()})
+    suggest = Completion(analyzer=ascii_fold)
+
+    def clean(self):
+        """
+        Automatically construct the suggestion input and weight by taking all
+        possible permutation of Person's name as ``input`` and taking their
+        popularity as ``weight``.
+        """
+        self.suggest = {
+            'input': [' '.join(p) for p in permutations(self.name.split())],
+        }
+
+
+class Person(Entity):
+    forename = Text(fields={'keywords': Keyword()})
+    surname = Text(fields={'keywords': Keyword()})
+
+    class Index:
+        name = 'person'
+        settings = {
+            'number_of_shards': 1,
+            'number_of_replicas': 0
+        }
+
+
+class Event(Entity):
+    date = Date()
+
+    class Index:
+        name = 'event'
+        settings = {
+            'number_of_shards': 1,
+            'number_of_replicas': 0
+        }
+
+
+class Organization(Entity):
+    class Index:
+        name = 'organization'
+        settings = {
+            'number_of_shards': 1,
+            'number_of_replicas': 0
+        }
+
+
+class Place(Entity):
+    location = GeoPoint()
+    desc = Text()
+    region = Text()
+    country = Text()
+
+    class Index:
+        name = 'place'
+        settings = {
+            'number_of_shards': 1,
+            'number_of_replicas': 0
+        }
