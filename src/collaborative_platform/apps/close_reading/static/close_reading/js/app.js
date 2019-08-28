82d0b918cf7150ad33796466efac8cd1785d4af7
Janchorizo <jancho@usal.es>
2019-08-28 16:37:18 +0200

    Add Annotator module to app.

--- src/collaborative_platform/apps/close_reading/static/close_reading/js/app.js
+++ src/collaborative_platform/apps/close_reading/static/close_reading/js/app.js
@@ -2,25 +2,27 @@ import PubSubChannel from './src/aux/pubsub.js';
 import AnnotatorWebSocket from './src/aux/websocket.js';
 import DocumentView from './src/document_view.js';
 import PanelView from './src/panel.js';
+import Annotator from './src/annotator.js';
 
 // Load all components
+const websocket = AnnotatorWebSocket();
 const channel = PubSubChannel.create();
 const document_view = DocumentView({channel});
 const panel_view = PanelView({channel});
+const annotator = Annotator({channel});
 
 // Create suscriber for sending messages using
 // the websocket
 const sub = {};
 channel.addToChannel(sub);
-sub.subscribe('websocket/send', json=>AnnotatorWebSocket.send(json));
+sub.subscribe('websocket/send', json=>console.log(json));
+sub.subscribe('websocket/send', json=>websocket.send(json));
 sub.subscribe('document/render', selection=>console.info('Document rendered.'));
-sub.subscribe('document/selection', selection=>console.log(selection));
 
 // Publish websocket updates
-AnnotatorWebSocket.addCallback('onload', file=>sub.publish('document/load', file));
-AnnotatorWebSocket.addCallback('onload', file=>console.log(file));
-AnnotatorWebSocket.addCallback('onreload', file=>sub.publish('document/load', file));
-AnnotatorWebSocket.addCallback('onreload', file=>console.log(file));
+websocket.addCallback('onload', file=>sub.publish('document/load', file));
+//AnnotatorWebSocket.addCallback('onload', file=>console.log(file));
+websocket.addCallback('onreload', file=>sub.publish('document/load', file));
 
 // Create websocket
-AnnotatorWebSocket.create();
+websocket.create();
