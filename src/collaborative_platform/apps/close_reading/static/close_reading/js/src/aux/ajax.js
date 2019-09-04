1d04af8df11d5e83477b4173124ddc6953922219
Janchorizo <jancho@usal.es>
2019-09-04 13:20:09 +0200

    Change 'AjaxCalls' module to perform calls using CSRF cookies.

--- src/collaborative_platform/apps/close_reading/static/close_reading/js/src/aux/ajax.js
+++ src/collaborative_platform/apps/close_reading/static/close_reading/js/src/aux/ajax.js
@@ -8,22 +8,67 @@
  * - get_autocomplete_url
  * */
 var AjaxCalls = function(args){
-	const base_url = [window.location.protocol + '/', window.location.hostname+':5000', 'api/v1'].join('/');
+	const base_url = [window.location.protocol + '/', window.location.hostname+':'+window.location.port, 'api/'].join('/');
 
-	const save_url = (project, file) => ['project', project, 'teiclose', file, 'save/'].join('/');
-	const annotate_url = (project, file) => ['project', project, 'teiclose', file, 'annotate/'].join('/');
-	const history_url = (project, file, version) => ['project', project, 'teiclose', file, version, 'annotationhistory/'].join('/');
+	const save_url = (project, file) => ['close_reading', 'project', project, 'file', file, 'save/'].join('/');
+	const history_url = (project, file, version) => ['close_reading','project', project, 'file', file,'version', version, 'history/'].join('/');
 	const search_url = (project, entity_type, query) => ['fuzzysearch', project, entity_type, query].join('/');
 
 	function _init(args){
 		const obj = {
-			get_save_url: (project, file) => base_url + '/' + save_url(project, file),
-			get_add_annotation_url: (project, file) => base_url + '/' + annotate_url(project, file),
-			get_history_url: (project, file, version) => base_url + '/' + history_url(project, file, version),
-			get_autocomplete_url: (project, entity_type, query) => base_url + '/' + search_url(project, entity_type, query)
+			safeFile: (project, file) => _createCall('PUT', base_url + save_url(project, file)),
+			getHistory: (project, file, version) => _createCall('GET', base_url + history_url(project, file, version)),
+			getAutocomplete: (project, entity_type, query) => _createCall('GET', base_url + search_url(project, entity_type, query))
 		};
 
 		return obj;
+	}
+
+	function _createCall(method,url){
+		function getCookie(name) {
+		    let cookieValue = null;
+		    if (document.cookie && document.cookie !== '') {
+		        const cookies = document.cookie.split(';');
+		        for (let i = 0; i < cookies.length; i++) {
+		            const cookie = cookies[i].trim();
+		            // Does this cookie string begin with the name we want?
+		            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+		                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+		                break;
+		            }
+		        }
+		    }
+		    return cookieValue;
+		}
+
+		const csrftoken = getCookie('csrftoken');
+
+		return new Promise(function(resolve, cancel){
+			function csrfSafeMethod(method) {
+			    // these HTTP methods do not require CSRF protection
+			    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+			}
+
+			$.ajaxSetup({
+			    beforeSend: function(xhr, settings) {
+			        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+			            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+			        }
+			    }
+			});
+
+			$.ajax({
+		        url: url,
+		        type: method,   //type is any HTTP method
+		        data: {},      //Data as js object
+		        success: function (a) {
+		            resolve({success:true, content:a});
+		        },
+		        error: function (a) {
+		            resolve({success:false, content:a});
+		        }
+		    })
+		})
 	}
 
 	function _notimplemented(method){
