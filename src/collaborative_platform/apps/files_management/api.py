b0413536b8d3a3715c466dc2298b87e5bc24b59e
virdiignis <gbryk1@gmail.com>
2019-09-05 10:11:27 +0200

    upload process change

--- src/collaborative_platform/apps/files_management/api.py
+++ src/collaborative_platform/apps/files_management/api.py
@@ -19,20 +19,14 @@ from .helpers import extract_text_and_entities, index_entities, upload_file, upl
 
 
 @login_required
-def upload(request):  # type: (HttpRequest) -> HttpResponse
+def upload(request, directory_id):  # type: (HttpRequest, int) -> HttpResponse
     if request.method == "POST" and request.FILES:
         files_list = request.FILES.getlist("files")
         if not files_list:
             return HttpResponseBadRequest("File not attached properly")
-        try:
-            project = int(request.POST.get("project"))
-            project = Project.objects.filter(id=project).get()
-        except (ValueError, Project.DoesNotExist):  # TODO check level of contribution to authorize user to upload files
-            return HttpResponseBadRequest("Invalid project id")
-        try:
-            parent_dir = int(request.POST.get("parent_dir"))
-        except ValueError:
-            parent_dir = None
+
+        parent_dir = directory_id
+        project = Directory.objects.filter(id=parent_dir).get().project
 
         upload_statuses = {}
 
