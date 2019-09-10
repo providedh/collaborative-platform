2e3ae3d4937583e967dd8ac7d8651eddf8f015ae
virdiignis <gbryk1@gmail.com>
2019-09-10 11:25:40 +0200

    fix for opening main dir of project

--- src/collaborative_platform/apps/files_management/helpers.py
+++ src/collaborative_platform/apps/files_management/helpers.py
@@ -120,6 +120,7 @@ def get_directory_content(dir, indent):  # type: (Directory, int) -> dict
     result['kind'] = "folder"
     result['icon'] = 'fa-file-folder-o'
     result['open'] = not indent
+    result['show'] = not indent
     result['indent'] = indent
 
     return result
