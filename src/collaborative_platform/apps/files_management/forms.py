c271876d445a4eba3414858dd495bc1b142ed1ca
virdiignis <gbryk1@gmail.com>
2019-09-10 13:20:52 +0200

    remove old upload view

--- src/collaborative_platform/apps/files_management/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class UploadFileForm(forms.Form):
-    title = forms.CharField(max_length=50)
-    project = forms.IntegerField()
-    parent_dir = forms.CharField()
-    file = forms.FileField()
--- src/collaborative_platform/apps/files_management/urls.py
+++ src/collaborative_platform/apps/files_management/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload/', views.upload, name='upload'),
     path('<int:file_id>/', views.file, name='file'),
     path('<int:file_id>/version/<int:version>/', views.file, name='file_version'),
     path('<int:file_id>/versions', views.fileversions, name='fileversions'),
