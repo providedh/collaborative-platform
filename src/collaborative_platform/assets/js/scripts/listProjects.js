db7ecd72ca9930701f824a2f53b26afac3ce0822
Bartosz Szymendera <bartszym@man.poznan.pl>
2019-09-11 16:17:15 +0200

    users project

--- src/collaborative_platform/assets/js/scripts/listProjects.js
+++ src/collaborative_platform/assets/js/scripts/listProjects.js
@@ -77,3 +77,43 @@ $('[js-listProjectsPublic]').DataTable( {
         $('.dataTables_paginate > .pagination').addClass('pagination-sm');
     }
 } );
+
+$('[js-listProjectsUsers]').DataTable( {
+    "ajax": {
+        "url": "/api/projects/get_users/" + $('[js-listProjectsUsers]').attr('data-user-id') + '/',
+        "dataSrc": "data"
+    },
+    "columns": [
+        {
+            "data": "title",
+            "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+                $(nTd).html('<a href="'+ $('[js-listProjectsUsers]').attr('data-link-project') + '' + oData.id +'">' + sData + '</a>');
+            }
+        },
+        {
+            "data": "contributors",
+            "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+                var html = '';
+
+                for (item in sData) {
+                    html += '<a href="'+ $('[js-listProjectsUsers]').attr('data-link-contributor') + '' + sData[item].id +'">' + sData[item].first_name + ' ' + sData[item].last_name + '</a>';
+                    if (item != sData.length - 1 ) {
+                        html += ', '
+                    }
+                }
+
+                $(nTd).html(html);
+            }
+        },
+        {
+            "data": "modification_date",
+            "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+                $(nTd).html(moment(sData).format('DD.MM.YY, HH:mm'));
+            }
+        }
+    ],
+
+    "drawCallback": function () {
+        $('.dataTables_paginate > .pagination').addClass('pagination-sm');
+    }
+} );
