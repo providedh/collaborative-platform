90020d5b62bc4ac83642ca0d84eb37576d9a9da9
Bartosz Szymendera <bartszym@man.poznan.pl>
2019-09-11 15:03:30 +0200

    PROVIDEDH-115

---
+++ src/collaborative_platform/assets/js/scripts/listFileVersions.js
@@ -0,0 +1,56 @@
+$('[js-listFileVersions]').DataTable( {
+    "searching": false,
+    "processing": true,
+    "serverSide": true,
+    "ordering": false,
+    "ajax": {
+        "url": "/api/files/" + $('[js-listFileVersions]').attr('data-file-id') + "/versions/",
+        "dataSrc": "data"
+    },
+    "columns": [
+        {
+            "data": "number",
+        },
+    
+        {
+            "data": "creation_date",
+            "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+                $(nTd).html(moment(sData).format('DD.MM.YY, HH:mm'));
+            }
+        },
+
+        {
+            "data": "id",
+            "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+                $(nTd).html('lll');
+            }
+        },
+
+        {
+            "data": "upload",
+            "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+                $(nTd).html('<a href="/' + oData.upload + '" class="tb-button"><i class="fa fa-download"></i></a>');
+            }
+        }
+
+        ,
+        {
+            "data": "hash",
+            "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+                $(nTd).html('<div class="input-group input-group-sm"><input type="text" readonly class="form-control" value="'+sData+'"/><div class="input-group-append"><button class="btn btn-outline-info" js-copyHash type="button"><i class="fa fa-copy"/></button></div></div>');
+            }
+        }
+    ],
+
+    "drawCallback": function () {
+        $('.dataTables_paginate > .pagination').addClass('pagination-sm');
+        $("[js-listRecentActivities] thead").remove();
+    }
+} );
+
+
+$(document).on('click', '[js-copyHash]', function() {
+    $(this).parent().siblings('input').select()
+    $(this).parent().siblings('input').setSelectionRange(0, 99999);
+    document.execCommand("copy");
+})
