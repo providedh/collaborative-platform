9933e814cd0a39178b0310a3bc67e267852206b3
Janchorizo <jancho@usal.es>
2019-09-15 23:28:15 +0200

    Migrate old code for auto-complete functionality.

--- src/collaborative_platform/apps/close_reading/static/close_reading/js/src/panel.js
+++ src/collaborative_platform/apps/close_reading/static/close_reading/js/src/panel.js
@@ -15,9 +15,13 @@
  * - panel/reset
  * - panel/autocomplete_options
  * */
-var PanelView = function(args){
+import AjaxCalls from './utilities/ajax.js';
+
+let PanelView = function(args){
 	let self = null;
+	const ajaxCalls = AjaxCalls();
 	let values = {};
+	let currentFocus;
 
 	function _init(args){
 		const obj = {
@@ -60,6 +64,18 @@ var PanelView = function(args){
 		document.getElementById('saveFile')
 			.addEventListener('click', ()=>obj.publish('annotator/save',{}));
 
+		document.getElementById('references-autocomplete').addEventListener('input', e=>{
+	        const tag = values['tag-name'];
+	        document.getElementById('references').value = e.target.value;
+	        if(values['attribute-name'] == 'sameAs' &&
+	            ['person', 'event', 'org', 'place'].includes(tag)){
+	            _updateAutocompleteOptions(tag.replace('org','organization'), e.target.value);
+	        }
+	    })
+
+	    document.getElementById("references-autocomplete").options = [];
+	    _setup_autocomplete(document.getElementById("references-autocomplete"));
+
 		obj.getValues = ()=>_getValues;
 
 		self = obj;
@@ -76,6 +92,8 @@ var PanelView = function(args){
         	.getElementById('asserted-value-input-options')
         	.getElementsByClassName('locus-' + values['locus'])[0]
         	.cloneNode(true);
+
+        input.id = 'asserted-value';
 
         input.addEventListener('change', ()=>_handleValueChange('asserted-value', input.value));
 
@@ -134,6 +152,120 @@ var PanelView = function(args){
 		return options;
 	}
 
+	function _setup_autocomplete(inp) {
+	        //Snippet from w3school.com	     
+	        inp.addEventListener("input", function(e) {
+	              let a, b, i, val = inp.value;
+	        closeAllLists();
+	        if (!val) { return false;}
+	        currentFocus = -1;
+	        a = document.createElement("DIV");
+	        a.setAttribute("id", "autocomplete-list");
+	        a.setAttribute("class", "autocomplete-items");
+	        document.getElementById('references-container').appendChild(a);
+	        for (let i = 0; i < inp.options.length; i++) {
+	            if (inp.options[i].name.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+	              b = document.createElement("DIV");
+	              b.data = inp.options[i];
+	              b.innerHTML = "<strong>" + inp.options[i].name.substr(0, val.length) + "</strong>";
+	              b.innerHTML += inp.options[i].name.substr(val.length);
+	              b.innerHTML += ` | ${inp.options[i].filepath}`;
+	              b.innerHTML += "<input type='hidden' id='"+inp.options[i].id+"' value='" + 
+	                `${inp.options[i].name} | ${inp.options[i].filepath}`; + "'>";
+	              b.addEventListener("click", function(e) {
+	                  document.getElementById('references').value = this.data.name;
+	                  inp.value = `${this.data.name} | ${this.data.filepath}`;
+	                  document.getElementById('proposedValue').value = this.data.id;
+	                  closeAllLists();
+	              });
+	              a.appendChild(b);
+	            }
+	        }
+	      });
+
+	      inp.addEventListener("keydown", function(e) {
+	          let x = document.getElementById("autocomplete-list");
+	          if (x) x = x.getElementsByTagName("div");
+	          if (e.keyCode == 40) {
+	            currentFocus++;
+	            addActive(x);
+	          } else if (e.keyCode == 38) { //up
+	            currentFocus--;
+	            addActive(x);
+	          } else if (e.keyCode == 13) {
+	            e.preventDefault();
+	            if (currentFocus > -1) {
+	              if (x) x[currentFocus].click();
+	            }
+	          }
+	      });
+	      function addActive(x) {
+	        if (!x) return false;
+	        removeActive(x);
+	        if (currentFocus >= x.length) currentFocus = 0;
+	        if (currentFocus < 0) currentFocus = (x.length - 1);
+	        x[currentFocus].classList.add("autocomplete-active");
+	      }
+	      function removeActive(x) {
+	        for (let i = 0; i < x.length; i++) {
+	          x[i].classList.remove("autocomplete-active");
+	        }
+	      }
+	      function closeAllLists(elmnt) {
+	        let x = document.getElementsByClassName("autocomplete-items");
+	        for (let i = 0; i < x.length; i++) {
+	          if (elmnt != x[i] && elmnt != inp) {
+	            x[i].parentNode.removeChild(x[i]);
+	          }
+	        }
+	      }
+	      document.addEventListener("click", function (e) {
+	          closeAllLists(e.target);
+	      });
+	    }
+
+	function _updateAutocompleteOptions(entityType, text){
+		ajaxCalls.getAutocomplete(window.project_id, entityType, text).then(response=>{
+			if(response.success === true){				
+				/*response.content.data.push({_source:{
+					name: 'Alex',
+					filepath: 'some.text',
+					id: '012'
+				}})*/
+				document.getElementById("references-autocomplete").options = response.content.data.map(a=>(
+	                {name: a._source.name, id:a._source.id, filepath:a._source.filepath}
+	            ))
+	            _updateAutocompleteInput(document.getElementById("references-autocomplete"));
+			}
+	        else
+	        	console.log('autocomplete - error < ',window.project, entityType, text,' < ',response)
+		});
+	}
+
+	function _updateAutocompleteInput(inp){
+	    currentFocus = -1;
+	    let a = document.getElementById('autocomplete-list');
+
+	    for (let i = 0; i < inp.options.length; i++) {
+	        if (inp.options[i].name.substr(0, inp.value.length).toUpperCase() == inp.value.toUpperCase()) {
+	          b = document.createElement("DIV");
+	          b.data = inp.options[i];
+	          b.innerHTML = "<strong>" + inp.options[i].name.substr(0, inp.value.length) + "</strong>";
+	          b.innerHTML += inp.options[i].name.substr(inp.value.length);
+	          b.innerHTML += ` | ${inp.options[i].filepath}`;
+	          b.innerHTML += "<input type='hidden' id='"+inp.options[i].id+"' value='" + 
+	            `${inp.options[i].name} | ${inp.options[i].filepath}` + "'>";
+	          b.addEventListener("click", function(e) {
+	              document.getElementById('references').value = this.data.name;
+	              inp.value = `${this.data.name} | ${this.data.filepath}`;
+	              document.getElementById(''asserted-value'').value = this.data.id;
+	              closeAllLists();
+	          });
+	          a.appendChild(b);
+	        }
+	    }
+	}
+
 	function _handleCreateTEIannotation(){
 		self.publish('annotator/create-tei', _getCurrentValues());
 	}
@@ -148,6 +280,8 @@ var PanelView = function(args){
 			_updateReferencesControl();
 		if(['locus', 'attribute-name', 'tei-tag-name'].includes(id))
 			_updateAssertedValueControl();
+		if(['locus', 'attribute-name'].includes(id))
+			_updateReferencesControl();
 		
 		console.log(id,value);
 	}
