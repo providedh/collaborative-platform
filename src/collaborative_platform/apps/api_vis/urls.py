1cb0f24e91d0b03ffe7aee560b4905416a0bb398
bug-rancher <bug-rancher@protonmail.com>
2019-09-17 14:33:44 +0200

    Add getting project history and project files to api for visualization

--- src/collaborative_platform/apps/api_vis/urls.py
+++ src/collaborative_platform/apps/api_vis/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('projects/', views.projects, name='projects'),
+    path('projects/<int:project_id>/history/', views.project_history, name='project_history'),
+    path('projects/<int:project_id>/files/', views.project_files, name='project_files'),
 ]
