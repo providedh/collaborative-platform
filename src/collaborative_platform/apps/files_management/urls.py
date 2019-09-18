c56a4e40f1fe23e1e2ba8110d14e11ddeb56e08b
bug-rancher <bug-rancher@protonmail.com>
2019-09-18 13:56:11 +0200

    Fix namespaces in user, file and file versions template

--- src/collaborative_platform/apps/files_management/urls.py
+++ src/collaborative_platform/apps/files_management/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('<int:file_id>/', views.file, name='file'),
     path('<int:file_id>/version/<int:version>/', views.file, name='file_version'),
-    path('<int:file_id>/versions', views.fileversions, name='fileversions'),
+    path('<int:file_id>/versions/', views.fileversions, name='fileversions'),
 ]
