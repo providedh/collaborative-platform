246feb31c1318bc16c33395bbddedbdac9907ecb
bug-rancher <bug-rancher@protonmail.com>
2019-09-24 11:24:43 +0200

    Add endpoint and implementation to api_vis for retrieve metadata from TEI header

--- src/collaborative_platform/apps/api_vis/urls_api.py
+++ src/collaborative_platform/apps/api_vis/urls_api.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('projects/', api.projects),
     path('projects/<int:project_id>/history/', api.project_history),
     path('projects/<int:project_id>/files/<int:file_id>/body/', api.file_body),
+    path('projects/<int:project_id>/files/<int:file_id>/meta/', api.file_meta),
     path('projects/<int:project_id>/files/<int:file_id>/', api.file),
     path('projects/<int:project_id>/files/', api.project_files),
 ]
