4dcb176445420fe8f25d6b3923ca36dfede65db9
bug-rancher <bug-rancher@protonmail.com>
2019-10-03 11:39:54 +0200

    Add returning uncertainties for annotations in get annotations request

--- src/collaborative_platform/apps/api_vis/helpers.py
+++ src/collaborative_platform/apps/api_vis/helpers.py
@@ -53,40 +53,79 @@ def get_annotations_from_file_version_body(file_version, namespaces, annotation_
     annotations = []
 
     tree = etree.fromstring(xml_content)
-    xml_body_nodes = tree.xpath('//default:text/default:body', namespaces=namespaces)[0]
+    xml_body_node = tree.xpath('//default:text/default:body', namespaces=namespaces)[0]
+    xml_certainties_node = tree.xpath('//default:teiHeader'
+                                      '//default:classCode[@scheme="http://providedh.eu/uncertainty/ns/1.0"]',
+                                      namespaces=namespaces)[0]
 
     for annotation_tag in annotation_tags:
-        annotation_nodes = xml_body_nodes.xpath('.//default:{0}'.format(annotation_tag), namespaces=namespaces)
+        annotation_nodes = xml_body_node.xpath('.//default:{0}'.format(annotation_tag), namespaces=namespaces)
 
-        for node in annotation_nodes:
+        for ann_node in annotation_nodes:
             attributes = []
+            uncertainties = []
 
-            for item in node.attrib.items():
-                name = item[0]
+            for ann_item in ann_node.attrib.items():
+                attribute = reformat_attribute(ann_item, namespaces)
+                attributes.append(attribute)
 
-                regex = r'{.*?}'
-                match = re.search(regex, name)
+                # check if annotation have uncertainty
+                regex = r'({.*?})*?id$'
+                match = re.search(regex, ann_item[0])
 
                 if match:
-                    for prefix, namespace in namespaces.items():
-                        ns = match.group()[1:-1]
+                    id_value = ann_item[1]
+                    xml_uncertainties = xml_certainties_node.xpath('./default:certainty[@target="#{0}"]'.format(id_value),
+                                                                   namespaces=namespaces)
 
-                        if namespace == ns:
-                            name = name.replace('{' + ns + '}', prefix + ':')
+                    for cert_node in xml_uncertainties:
+                        cert_attributes = []
 
-                atribute = {
-                    'name': name,
-                    'value': item[1]
-                }
+                        for cert_item in cert_node.attrib.items():
+                            cert_attribute = reformat_attribute(cert_item, namespaces)
+                            cert_attributes.append(cert_attribute)
 
-                attributes.append(atribute)
+                        description = cert_node.xpath('./default:description', namespaces=namespaces)
+
+                        if description:
+                            description = description.text
+
+                        uncertainty = {
+                            'tag': 'certainty',
+                            'attributes': cert_attributes if cert_attributes else None,
+                            'description': description if description else None,
+                        }
+
+                        uncertainties.append(uncertainty)
 
             annotation = {
                 'tag': annotation_tag,
-                'attributes': attributes,
-                'textContext': node.text,
+                'attributes': attributes if attributes else None,
+                'textContext': ann_node.text if ann_node.text else None,
+                'uncertainties': uncertainties if uncertainties else None,
             }
 
             annotations.append(annotation)
 
     return annotations
+
+
+def reformat_attribute(item, namespaces):
+    name = item[0]
+
+    regex = r'{.*?}'
+    match = re.search(regex, name)
+
+    if match:
+        for prefix, namespace in namespaces.items():
+            ns = match.group()[1:-1]
+
+            if namespace == ns:
+                name = name.replace('{' + ns + '}', prefix + ':')
+
+    attribute = {
+        'name': name,
+        'value': item[1]
+    }
+
+    return attribute
