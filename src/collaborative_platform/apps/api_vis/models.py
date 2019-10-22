eb2466fb5a481dbb4d3ae7d9058f8ec73299529a
virdiignis <gbryk1@gmail.com>
2019-10-22 11:30:09 +0200

    entities models

--- src/collaborative_platform/apps/api_vis/models.py
+++ src/collaborative_platform/apps/api_vis/models.py
@@ -1,3 +1,36 @@
+from django.contrib.gis.db.models import PointField
 from django.db import models
 
-# Create your models here.
+from apps.files_management.models import File, FileVersion
+from apps.projects.models import Project
+
+
+class Entity(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    fileversion = models.ForeignKey(FileVersion, on_delete=models.CASCADE)
+    id = models.CharField(max_length=255)
+    name = models.TextField()
+    xml = models.TextField()
+    context = models.TextField()
+
+    class Meta:
+        abstract = True
+        unique_together = ("fileversion", "id")
+
+
+class Person(Entity):
+    forename = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+
+
+class Event(Entity):
+    date = models.DateTimeField()
+
+
+class Organization(Entity):
+    pass
+
+
+class Place(Entity):
+    location = PointField(geography=True)
+    country = models.CharField(max_length=255)
