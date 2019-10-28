ac3bce39f63107c1cf7f1b333d1edcc3dd2d0e4f
Janchorizo <jancho@usal.es>
2019-10-28 13:49:18 +0100

    Create dynamic charts for each tag attribute.

--- src/collaborative_platform/apps/dataset_stats/assets/src/index.js
+++ src/collaborative_platform/apps/dataset_stats/assets/src/index.js
@@ -5,6 +5,7 @@ import App from "./components/App";
 render(<App />, document.getElementById('react-root'));*/
 
 //alert('hi');
+import Chart from 'chart.js';
 
 function toggle(e){
   e.addEventListener('click',evt=>{
@@ -16,3 +17,38 @@ function toggle(e){
 Array
   .from(document.getElementsByClassName('entitySummary'))
   .forEach(toggle)
+
+function renderChart(node){
+	const id = node.id;
+	const data_str = node.attributes['data'].value.replace(/'/g, '"');
+	const data = JSON.parse(data_str);
+
+	node.setAttribute('height', 100 + (25*data.length))
+
+	const ctx = document.getElementById(id).getContext('2d');
+	const myChart = new Chart(ctx, {
+	    type: 'horizontalBar',
+	    data: {
+	        labels: data.map(x=>x[0]),
+	        datasets: [{
+	            label: '# of Votes',
+	            data: data.map(x=>x[1]),
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	    	responsive: false,
+	        scales: {
+	            xAxes: [{
+	                ticks: {
+	                    beginAtZero: true
+	                }
+	            }],
+	        }
+	    }
+	});
+}
+
+Array
+  .from(document.getElementsByClassName('chartContainer'))
+  .forEach(renderChart)
--- src/collaborative_platform/apps/dataset_stats/views.py
+++ src/collaborative_platform/apps/dataset_stats/views.py
@@ -13,87 +13,87 @@ def main(request, project_id=1):  # type: (HttpRequest, int, int) -> HttpRespons
 
     tags = [
     	{
-    		"name": 'person',
+    		"name": "person",
     		"count": 99,
     		"coverage": 100,
     		"attributes": [
     			{
-    				"name": 'gender',
+    				"name": "gender",
     				"top_perc": 60,
-    				"top_value": '1',
+    				"top_value": "1",
     				"coverage": 90,
     				"values": [
-    					['1', 60],
-    					['2', 39],
+    					["1", 60],
+    					["2", 39],
     				]
     			}
     		]
     	},
     	{
-    		"name": 'placeName',
+    		"name": "placeName",
     		"count": 190,
     		"coverage": 30,
     		"attributes": []
     	},
     	{
-    		"name": 'certainty',
+    		"name": "certainty",
     		"coverage": 20,
     		"count": 45,
     		"attributes": [
     			{
-    				"name": 'locus',
+    				"name": "locus",
     				"top_perc": 70,
-    				"top_value": 'value',
+    				"top_value": "value",
     				"coverage": 100,
     				"values": [
-    					['value', 40],
-    					['attribute', 10],
-    					['"name"', 20]
+    					["value", 40],
+    					["attribute", 10],
+    					["name", 20]
     				]
     			},
     			{
-    				"name": 'cert',
+    				"name": "cert",
     				"top_perc": 50,
-    				"top_value": 'medium',
+    				"top_value": "medium",
     				"coverage": 100,
     				"values": [
-    					['low', 10],
-    					['medium', 25],
-    					['high', 5],
-    					['unknown', 10]
+    					["low", 10],
+    					["medium", 25],
+    					["high", 5],
+    					["unknown", 10]
     				]
     			},
     			{
-    				"name": 'type',
+    				"name": "type",
     				"top_perc": 90,
-    				"top_value": 'ignorance',
+    				"top_value": "ignorance",
     				"coverage": 100,
     				"values": [
-    					['ignorance', 90],
-    					['credibility', 5],
-    					['incompletness', 5]
+    					["ignorance", 90],
+    					["credibility", 5],
+    					["incompletness", 5]
     				]
     			},
     			{
-    				"name": 'tag',
+    				"name": "tag",
     				"top_perc": 40,
-    				"top_value": 'person',
+    				"top_value": "person",
     				"coverage": 100,
     				"values": [
-    					['person', 40],
-    					['placeName', 5],
-    					['date', 20],
-    					['location', 20],
-    					['age', 15]
+    					["person", 40],
+    					["placeName", 5],
+    					["date", 20],
+    					["location", 20],
+    					["age", 15]
     				]
     			},
     			{
-    				"name": 'attribute',
+    				"name": "attribute",
     				"top_perc": 100,
-    				"top_value": 'gender',
+    				"top_value": "gender",
     				"coverage": 5,
     				"values": [
-    					['gender', 2]
+    					["gender", 2]
     				]
     			}
     		]
@@ -101,10 +101,10 @@ def main(request, project_id=1):  # type: (HttpRequest, int, int) -> HttpRespons
     ]
 
     content = {
-    	'title':project.title, 
-    	'project_id':project_id,
-    	'DEVELOPMENT':True,
-    	'tags': tags,
-    	'document_count': 200,
+    	"title":project.title, 
+    	"project_id":project_id,
+    	"DEVELOPMENT":True,
+    	"tags": tags,
+    	"document_count": 200,
     }
-    return render(request, 'dataset_stats/app.html', content)
+    return render(request, "dataset_stats/app.html", content)
