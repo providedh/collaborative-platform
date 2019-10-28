e82b394cdfb25707de605d5e2d90e3027f8b0e73
Janchorizo <jancho@usal.es>
2019-10-28 11:58:04 +0100

    Add test placeholder data for stat summary.

--- src/collaborative_platform/apps/dataset_stats/views.py
+++ src/collaborative_platform/apps/dataset_stats/views.py
@@ -10,9 +10,100 @@ from apps.projects.models import Project
 @user_has_access()
 def main(request, project_id=1):  # type: (HttpRequest, int, int) -> HttpResponse
     project = Project.objects.get(pk=project_id)
+
+    tags = [
+    	{
+    		"name": 'person',
+    		"count": 99,
+    		"coverage": 100,
+    		"attributes": [
+    			{
+    				"name": 'gender',
+    				"top_perc": 60,
+    				"top_value": '1',
+    				"coverage": 90,
+    				"values": [
+    					['1', 60],
+    					['2', 39],
+    				]
+    			}
+    		]
+    	},
+    	{
+    		"name": 'place"Name"',
+    		"count": 190,
+    		"coverage": 30,
+    		"attributes": []
+    	},
+    	{
+    		"name": 'certainty',
+    		"coverage": 20,
+    		"count": 45,
+    		"attributes": [
+    			{
+    				"name": 'locus',
+    				"top_perc": 70,
+    				"top_value": 'value',
+    				"coverage": 100,
+    				"values": [
+    					['value', 40],
+    					['attribute', 10],
+    					['"name"', 20]
+    				]
+    			},
+    			{
+    				"name": 'cert',
+    				"top_perc": 50,
+    				"top_value": 'medium',
+    				"coverage": 100,
+    				"values": [
+    					['low', 10],
+    					['medium', 25],
+    					['high', 5],
+    					['unknown', 10]
+    				]
+    			},
+    			{
+    				"name": 'type',
+    				"top_perc": 90,
+    				"top_value": 'ignorance',
+    				"coverage": 100,
+    				"values": [
+    					['ignorance', 90],
+    					['credibility', 5],
+    					['incompletness', 5]
+    				]
+    			},
+    			{
+    				"name": 'tag',
+    				"top_perc": 40,
+    				"top_value": 'person',
+    				"coverage": 100,
+    				"values": [
+    					['person', 40],
+    					['place"Name"', 5],
+    					['date', 20],
+    					['location', 20],
+    					['age', 15]
+    				]
+    			},
+    			{
+    				"name": 'attribute',
+    				"top_perc": 100,
+    				"top_value": 'gender',
+    				"coverage": 5,
+    				"values": [
+    					['gender', 2]
+    				]
+    			}
+    		]
+    	},
+    ]
+
     content = {
     	'title':project.title, 
     	'project_id':project_id,
-    	'DEVELOPMENT':True
+    	'DEVELOPMENT':True,
+    	'tags': tags,
     }
     return render(request, 'dataset_stats/app.html', content)
