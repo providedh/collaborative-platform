c6f811b5cde3a6ffaa190e21963ba64136a14dc7
Janchorizo <jancho@usal.es>
2020-01-13 11:14:37 +0100

    Change how the saved configuration is loaded in the application.

--- src/collaborative_platform/apps/vis_dashboard/assets/src/index.js
+++ src/collaborative_platform/apps/vis_dashboard/assets/src/index.js
@@ -2,12 +2,8 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./components/App_test";
 
-window.dashboardSavedConf = JSON.parse('{"views":[{"type":"Histogram","config":{"barDirection":"Horizontal","dimension":"Number of entities per document","renderOverlay":true,"overlay":"Certainty level"},"id":"1"}],"layout":[{"w":8,"h":7,"x":0,"y":0,"i":"1","minW":4,"minH":4,"moved":false,"static":false,"isDraggable":true,"isResizable":true}]}')
-
-const dashboardConfig = window.hasOwnProperty('dashboardSavedConf')?
-    window.dashboardSavedConf:{
-    views:[],
-    layout:[]
-};
+const dashboardConfig = typeof window.hasOwnProperty('config')?
+    Object.assign({views:[], layout:[], authors:[], version: '',},window.config):
+    {views:[], layout:[], authors:[], version: '',};
 
 render(<App savedConf={dashboardConfig} />, document.getElementById('react-root'));
