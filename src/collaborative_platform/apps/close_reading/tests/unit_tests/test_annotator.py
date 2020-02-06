a45620928c9aaf3ac2a383d745a56b46fd376c3a
bug-rancher <bug-rancher@protonmail.com>
2020-02-06 09:25:41 +0100

    Fix test 4th for Annotator

--- src/collaborative_platform/apps/close_reading/tests/unit_tests/test_annotator.py
+++ src/collaborative_platform/apps/close_reading/tests/unit_tests/test_annotator.py
@@ -89,30 +89,30 @@ class TestAnnotator:
             result = annotator.add_annotation(input_text, file_id, json, user_guid)
 
         assert str(exception.value) == "This tag already exist."
-#
-#     def test_add_annotation__add_tag_to_text__fragment_with_same_tag_and_certainty__exception(self, mock_get_user_data_from_db):
-#         json = {
-#             "start_row": 219,
-#             "start_col": 115,
-#             "end_row": 219,
-#             "end_col": 121,
-#             "tag": "date"
-#         }
-#
-#         input_file_path = os.path.join(DIRNAME, "test_annotator_files", "source_files", "source_file.xml")
-#
-#         input_text = read_file(input_file_path)
-#
-#         user_guid = 'abcde'
-#
-#         input_text = input_text.decode('utf-8')
-#
-#         with assert_raises(NotModifiedException) as exception:
-#             annotator = Annotator()
-#             result = annotator.add_annotation(input_text, json, user_guid)
-#
-#         assert exception.exception.message == "This tag already exist."
-#
+
+    @pytest.mark.django_db
+    def test_add_annotation__add_tag_to_text__fragment_with_same_tag_and_certainty__exception(self):
+        json = {
+            "start_row": 222,
+            "start_col": 127,
+            "end_row": 222,
+            "end_col": 133,
+            "tag": "date"
+        }
+
+        input_file_path = os.path.join(DIRNAME, "test_files", "source_files", "source_file.xml")
+
+        input_text = read_file(input_file_path)
+
+        user_guid = 'abcde'
+        file_id = 1
+
+        with pytest.raises(NotModified) as exception:
+            annotator = Annotator()
+            result = annotator.add_annotation(input_text, file_id, json, user_guid)
+
+        assert str(exception.value) == "This tag already exist."
+
 #     def test_add_annotation__add_certainty_without_tag_to_text__fragment_without_tag__string(self, mock_get_user_data_from_db):
 #         json = {
 #             "start_row": 218,
