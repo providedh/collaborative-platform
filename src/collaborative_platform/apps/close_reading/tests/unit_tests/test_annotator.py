856055867693b404b57805f0cdb4b488eb316699
bug-rancher <bug-rancher@protonmail.com>
2020-02-10 09:35:23 +0100

    Fix test 17th for Annotator

--- src/collaborative_platform/apps/close_reading/tests/unit_tests/test_annotator.py
+++ src/collaborative_platform/apps/close_reading/tests/unit_tests/test_annotator.py
@@ -469,38 +469,36 @@ class TestAnnotator:
 
         assert result == expected_text
 
-#     def test_add_annotation__add_certainty_to_tag__fragment_with_same_tag_and_other_certainty__string(self, mock_get_user_data_from_db):
-#         json = {
-#             "start_row": 219,
-#             "start_col": 475,
-#             "end_row": 219,
-#             "end_col": 482,
-#             "category": "ignorance",
-#             "locus": "name",
-#             "certainty": "high",
-#             "asserted_value": "Pepper",
-#             "description": "awesome description",
-#             "tag": "name"
-#         }
-#
-#         input_file_path = os.path.join(DIRNAME, "test_annotator_files", "source_files", "source_file.xml")
-#         expected_file_path = os.path.join(DIRNAME, "test_annotator_files", "result_files",
-#                                           "add_certainty_to_tag__fragment_with_same_tag_and_other_certainty__result.xml")
-#
-#         input_text = read_file(input_file_path)
-#         expected_text = read_file(expected_file_path)
-#
-#         user_guid = 'abcde'
-#
-#         input_text = input_text.decode('utf-8')
-#
-#         annotator = Annotator()
-#         result = annotator.add_annotation(input_text, json, user_guid)
-#
-#         result = result.encode('utf-8')
-#
-#         assert result == expected_text
-#
+    @pytest.mark.django_db
+    def test_add_annotation__add_certainty_to_tag__fragment_with_same_tag_and_other_certainty__string(self):
+        json = {
+            "start_row": 222,
+            "start_col": 127,
+            "end_row": 222,
+            "end_col": 133,
+            "categories": ["ignorance"],
+            "locus": "name",
+            "certainty": "high",
+            "asserted_value": "person",
+            "description": "awesome description",
+            "tag": "date"
+        }
+
+        input_file_path = os.path.join(DIRNAME, "test_files", "source_files", "source_file.xml")
+        expected_file_path = os.path.join(DIRNAME, "test_files", "result_files",
+                                          "add_certainty_to_tag__fragment_with_same_tag_and_other_certainty__result.xml")
+
+        input_text = read_file(input_file_path)
+        expected_text = read_file(expected_file_path)
+
+        user_id = 2
+        file_id = 1
+
+        annotator = Annotator()
+        result = annotator.add_annotation(input_text, file_id, json, user_id)
+
+        assert result == expected_text
+
 #     def test_add_annotation__add_certainty_to_tag__fragment_with_same_tag_and_same_certainty__exception(self, mock_get_user_data_from_db):
 #         json = {
 #             "start_row": 219,
