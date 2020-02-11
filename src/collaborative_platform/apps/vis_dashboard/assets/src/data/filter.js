6885e888ff7c8b4042b2c689a74bf4126531a7c0
Janchorizo <jancho@usal.es>
2020-02-11 09:23:15 +0100

    Add Filter class.

---
+++ src/collaborative_platform/apps/vis_dashboard/assets/src/data/filter.js
@@ -0,0 +1,28 @@
+/* Class: Filter
+ *
+ * Represents a single filter applied to data dimension.
+ * 
+ * */
+export default function Filter(dim, id){
+	function _init(dim, id){
+		self = {dim, id};
+		
+		self.toString = ()=>JSON.stringify({dim, id})
+
+		return self;
+	}
+
+	return _init(dim, id);
+}
+
+Filter.prototype.fromString = function(json){
+	let filter = null;
+	try{
+		const {dim, id} = JSON.parse(json);
+		filter = Filter(dim, id);
+	}catch{
+		throw('Not able to parse json :'+json);
+	}
+
+	return filter;
+}
