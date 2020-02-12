58d43e458c2cb1f569dd0fc00455482d2725c722
bug-rancher <bug-rancher@protonmail.com>
2020-02-12 12:42:45 +0100

    Fix tests 32-36 for Annotator

--- src/collaborative_platform/apps/close_reading/tests/unit_tests/test_annotator.py
+++ src/collaborative_platform/apps/close_reading/tests/unit_tests/test_annotator.py
@@ -1076,81 +1076,84 @@ class TestAnnotator:
 
         assert result == expected_text
 
-#     test_data_add_annotation__wrong_request_parameters = [
-#         (
-#             "_empty_parameters",
-#             {
-#                 "start_row": 54,
-#                 "start_col": 7,
-#                 "end_row": 54,
-#                 "end_col": 11,
-#                 "category": "",
-#                 "locus": "",
-#                 "certainty": "",
-#                 "asserted_value": "",
-#                 "description": "",
-#                 "tag": ""
-#             },
-#             ValueError,
-#             "There is no method to modify xml according to given parameters."
-#         ),
-#         (
-#             "_start_pos_is_greater_than_end_pos",
-#             {
-#                 "start_pos": 8440,
-#                 "end_pos": 8435,
-#                 "tag": "test"
-#             },
-#             ValueError,
-#             "Start position of annotating fragment is greater or equal to end position."
-#         ),
-#         (
-#             "_no_position_arguments",
-#             {
-#                 "tag": "test"
-#             },
-#             ValueError,
-#             "No position arguments in request."
-#         ),
-#         (
-#             "_position_is_not_a_integer",
-#             {
-#                 "start_row": 54.15,
-#                 "start_col": 7,
-#                 "end_row": 54,
-#                 "end_col": 11,
-#                 "tag": "test"
-#             },
-#             TypeError,
-#             "Value of 'start_row' is not a integer."
-#         ),
-#         (
-#             "_position_is_not_a_positive_number",
-#             {
-#                 "start_row": 54,
-#                 "start_col": -7,
-#                 "end_row": 54,
-#                 "end_col": 11,
-#                 "tag": "test"
-#             },
-#             ValueError,
-#             "Value of 'start_col' must be a positive number."
-#         ),
-#     ]
-#
-#     @parameterized.expand(test_data_add_annotation__wrong_request_parameters)
-#     def test_add_annotation__wrong_request_parameters__exception(self, mock_get_user_data_from_db, _, json, error_type, error_message):
-#         input_file_path = os.path.join(DIRNAME, "test_annotator_files", "source_files",
-#                                        "source_file_without_annotators_and_certainties.xml")
-#
-#         input_text = read_file(input_file_path)
-#
-#         user_guid = 'abcde'
-#
-#         input_text = input_text.decode('utf-8')
-#
-#         with assert_raises(error_type) as error:
-#             annotator = Annotator()
-#             result = annotator.add_annotation(input_text, json, user_guid)
-#
-#         assert error.exception.message == error_message
+    test_parameters_list = [
+        (
+            # Empty parameters
+            {
+                'start_row': 54,
+                'start_col': 7,
+                'end_row': 54,
+                'end_col': 11,
+                'category': '',
+                'locus': '',
+                'certainty': '',
+                'asserted_value': '',
+                'description': '',
+                'tag': '',
+            },
+            BadRequest,
+            "There is no method to modify xml according to given parameters.",
+        ),
+        (
+            # start_pos is greater than end_pos
+            {
+                'start_pos': 8440,
+                'end_pos': 8435,
+                'tag': 'test',
+            },
+            BadRequest,
+            "Start position of annotating fragment is greater or equal to end position.",
+        ),
+        (
+            # No position arguments
+            {
+                'tag': 'test',
+            },
+            BadRequest,
+            "Reference parameters not provided. Provide 'target' parameter for reference to xml element, OR set of "
+            "positional parameters for reference to text fragment.",
+        ),
+        (
+            # Position is not a integer
+            {
+                'start_row': 54.15,
+                'start_col': 7,
+                'end_row': 54,
+                'end_col': 11,
+                'tag': 'test',
+            },
+            BadRequest,
+            "Value of 'start_row' is not a integer."
+        ),
+        (
+            # Position is not a positive_number
+            {
+                'start_row': 54,
+                'start_col': -7,
+                'end_row': 54,
+                'end_col': 11,
+                'tag': 'test',
+            },
+            BadRequest,
+            "Value of 'start_col' must be a positive number."
+        ),
+    ]
+
+    test_parameters_names = "json, error_type, error_message"
+
+    @pytest.mark.django_db
+    @pytest.mark.parametrize(test_parameters_names, test_parameters_list)
+    def test_add_annotation__wrong_request_parameters__exception(self, json, error_type, error_message):
+        input_file_path = os.path.join(DIRNAME, 'test_files', 'source_files',
+                                       'source_file_without_annotators_and_certainties.xml')
+
+        input_text = read_file(input_file_path)
+
+        user_id = 2
+        file_id = 1
+
+        with pytest.raises(error_type) as exception:
+            annotator = Annotator()
+            result = annotator.add_annotation(input_text, file_id, json, user_id)
+
+        assert str(exception.value) == error_message
