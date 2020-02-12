71c77439a5f0a93844919204b8bf22c1b1e86eea
Janchorizo <jancho@usal.es>
2020-02-12 09:27:01 +0100

    Add selected dimension as the visualization frame title.

--- src/collaborative_platform/apps/vis_dashboard/assets/src/components/core/VisFrame/VisFrame.js
+++ src/collaborative_platform/apps/vis_dashboard/assets/src/components/core/VisFrame/VisFrame.js
@@ -3,9 +3,16 @@ import styles from './style.module.css';
 import css from './style.css';
 
 export default function VisFrame({index, focused, onViewFocus, onViewClose, children}) {
+    const getTitlePadding = title=>({'paddingLeft': 'calc(50% - '+((children.props.dimension.length/2)*5.5)+'px)'});
+    
     return(
         <div className={`border ${styles.frame} ${focused?'focused border-primary':''}`}>
             <div className={styles.topbar}>
+                {children.props.hasOwnProperty('dimension')?
+                <span style={getTitlePadding(children.props.dimension)}>
+                    {children.props.dimension}
+                </span>
+                :''}
                 <button onClick={()=>onViewClose(index)} type="button" className="close" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
