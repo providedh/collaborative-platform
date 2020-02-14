571faf132e3ffcf3aa1e35edad89e51763296cb4
virdiignis <gbryk1@gmail.com>
2020-02-14 10:28:56 +0100

    test

--- src/collaborative_platform/setup.py
+++ src/collaborative_platform/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='collaborative-platform',
-    version='',
+    version='1',
     packages=['apps', 'apps.core', 'apps.core.migrations', 'apps.api_vis', 'apps.api_vis.migrations', 'apps.projects',
               'apps.projects.migrations', 'apps.close_reading', 'apps.close_reading.migrations', 'apps.vis_dashboard',
               'apps.vis_dashboard.migrations', 'apps.files_management', 'apps.files_management.migrations',
