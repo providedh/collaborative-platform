d109ecee886a3d630a20c69e605a845a1eb10b62
virdiignis <gbryk1@gmail.com>
2020-02-17 16:44:19 +0100

    test

--- src/collaborative_platform/collaborative_platform/settings_for_tests.py
+++ src/collaborative_platform/collaborative_platform/settings_for_tests.py
@@ -3,16 +3,13 @@ from .settings import *
 # File storage
 MEDIA_ROOT = os.path.join(BASE_DIR, '..', '..', 'media_for_tests')
 
-DATABASES['default']['HOST'] = "localhost"
-DATABASES['default']['USER'] = "postgres"
-DATABASES['default']['PASSWORD'] = ""
-# {
-#     'default': {
-#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-#         'NAME': 'postgres',
-#         # 'USER': 'postgres',
-#         # 'PASSWORD': '',
-#         'HOST': "localhost",
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': "localhost",
+        'PORT': '5432',
+    }
+}
