8bd8e7f614a123d6b156ff505d867e0af77392c2
virdiignis <gbryk1@gmail.com>
2020-02-26 16:03:06 +0100

    new taxonomy models

--- src/collaborative_platform/apps/projects/models.py
+++ src/collaborative_platform/apps/projects/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.postgres import fields
 
 from apps.core.models import Profile
 
@@ -77,40 +78,19 @@ class ProjectVersion(models.Model):
 
 class Taxonomy(models.Model):
     project = models.OneToOneField(Project, primary_key=True, related_name='taxonomy', on_delete=models.CASCADE)
-    xml_id_1 = models.CharField(max_length=255)
-    name_1 = models.CharField(max_length=255)
-    desc_1 = models.TextField(null=True, blank=True)
-    color_1 = models.CharField(max_length=7)
-    xml_id_2 = models.CharField(max_length=255)
-    name_2 = models.CharField(max_length=255)
-    desc_2 = models.TextField(null=True, blank=True)
-    color_2 = models.CharField(max_length=7)
-    xml_id_3 = models.CharField(max_length=255)
-    name_3 = models.CharField(max_length=255)
-    desc_3 = models.TextField(null=True, blank=True)
-    color_3 = models.CharField(max_length=7)
-    xml_id_4 = models.CharField(max_length=255)
-    name_4 = models.CharField(max_length=255)
-    desc_4 = models.TextField(null=True, blank=True)
-    color_4 = models.CharField(max_length=7)
     contents = models.TextField()
 
-    def save(self, *args, **kwargs):
-        if self.pk is not None:
-            from .taxonomy_template import template_string
-            self.contents = template_string.format(
-                self.project.title,
-                self.xml_id_1,
-                self.name_1,
-                self.desc_1,
-                self.xml_id_2,
-                self.name_2,
-                self.desc_2,
-                self.xml_id_3,
-                self.name_3,
-                self.desc_3,
-                self.xml_id_4,
-                self.name_4,
-                self.desc_4,
-            )
-        super(Taxonomy, self).save(*args, **kwargs)
+
+class UncertaintyCategory(models.Model):
+    taxonomy = models.ForeignKey(Taxonomy, related_name="categories", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    xml_id = models.CharField(max_length=255)
+    color = models.CharField(max_length=7)
+    desc = models.TextField(null=True, blank=True)
+
+
+class EntitySchema(models.Model):
+    taxonomy = models.ForeignKey(Taxonomy, related_name="entities", on_delete=models.CASCADE)
+    color = models.CharField(max_length=7)
+    icon = models.CharField(max_length=12)
+    properties = fields.JSONField()
