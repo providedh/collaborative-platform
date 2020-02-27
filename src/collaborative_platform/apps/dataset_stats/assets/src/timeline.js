cd368c65da522ce9165c0d800b68ffff28586f2e
Janchorizo <jancho@usal.es>
2020-02-27 10:00:07 +0100

    Manage position indexes based on time spans between modifications.

--- src/collaborative_platform/apps/dataset_stats/assets/src/timeline.js
+++ src/collaborative_platform/apps/dataset_stats/assets/src/timeline.js
@@ -20,6 +20,50 @@ export default function Timeline(args){
 		return self;
 	}
 
+	function _days_difference(t1, t2){
+	  	const milisInDay = 8.64e7;
+	  	// expected YYYY-MM-DD
+	  	const get_date = t=>new Date(
+		    +(t.slice(0,4)), // year
+		    +(t.slice(5,7)), // month
+		    +(t.slice(8,10)), // day
+		    0, // hours
+		    0, // minutes
+		    0, // seconds
+		    0); //miliseconds
+
+		const milis1 = get_date(t1).getTime(),
+		    milis2 = get_date(t2).getTime(),
+		    milisDiff = Math.abs(milis2-milis1),
+		    daysDiff = milisDiff / milisInDay;
+	  
+		return daysDiff;
+	}
+
+	function _processVersions(versions){
+		versions.forEach((x, i)=>x.i=i);
+		const dates = {};
+
+		for(let i=0; i<versions.length; i++){
+		 	const date = versions[i].date.slice(0, 10);
+		  
+		  	if(!dates.hasOwnProperty(date)){
+		    	let days = 0;
+		    	if(i > 0){
+		      		days = _days_difference(versions[i-1].date, versions[i].date);
+		      		if(days > 1){
+		      		  	for(let j=i; j<versions.length; j++){
+		      		    	versions[j].i = versions[j].i + 5;
+		      		  	}
+		      		}
+		    	}
+		    	dates[date] = {days, i: versions[i].i};
+		  	}
+		}
+
+		return [versions, dates];
+	}
+
 	function _setupContainer(){
 		const paddingStyle = window.getComputedStyle(self._container)['padding'],
 			paddingPixels = +(paddingStyle.slice(0, -2));
@@ -41,52 +85,22 @@ export default function Timeline(args){
 		self._legendG = self._svg.append('svg:g');
 	}
 
-	function _setupTimeline(){
-		const x = d3.scaleTime().range([0, self._width]),  
-    		xAxis = d3.axisTop().scale(x).tickSize(10, 0).tickPadding(6); 
-
-
-		self._svg.append('svg:g')
-			.classed('x', true)
-			.classed('axis', true)
-			.attr("transform", "translate(0," + 30 + ")");;
-
-		self._svg.append('svg:rect').attr("class", "pane")
-		    .attr("width", self._width)
-		    .attr("height", self._height)
-		    .attr('fill', 'none')
-		    .call(d3.zoom().on("zoom", zoom));
-		    
-		    x.domain([new Date(1999, 0, 1), new Date(2014, 0, 0)]);
-
-		    draw();
-
-		    function draw() {
-		      console.log ("drawing");
-		      self._svg.select("g.x.axis").call(xAxis);
-		    }
-
-		    function zoom() {
-		      console.log("zooming");
-		      self._svg.attr("transform", d3.event.transform);
-		      draw();
-		    }
+	function _setupScales(){
+		self._xScale = d3.scaleLinear()
+			.domain([0, self._versions[self._versions.length-1].i])
+			.range([0, self._width]);
 	}
 
 	function _renderProjectVersions(projectVersions){
 		self._fileVersionSide = 20;
 		self._fileVersionPadding = 5;
-		const xScale = d3.scaleLinear()
-			.domain([0, projectVersions.length])
-			.range([0, self._width]);
-		self._xScale = xScale;
 
-		let pVersions = self._versionsG.selectAll('g.pVersion').data(projectVersions);
+		let pVersions = self._versionsG.selectAll('g.pVersion').data(self._versions);
 		pVersions.exit().remove();
 		pVersions.enter().append('g').attr('class', 'pVersion');
 
 		self._pVersions = self._versionsG.selectAll('g.pVersion')
-			.attr('transform', (d,i)=>`translate(${xScale(i)}, 0)`);
+			.attr('transform', d=>`translate(${self._xScale(d.i)}, 0)`);
 
 		let fVersions = self._pVersions.selectAll('rect.fVersion').data(d=>d.files);
 		fVersions.exit().remove();
@@ -107,12 +121,8 @@ export default function Timeline(args){
 			.attr('fill', d=>colorScale(d.author));
 
 		const legendOrdinal = d3.legendColor()
-		  //d3 symbol creates a path-string, for example
-		  //"M0,-8.059274488676564L9.306048591020996,
-		  //8.059274488676564 -9.306048591020996,8.059274488676564Z"
 		  .orient('horizontal')
 		  .shapePadding(10)
-		  //use cellFilter to hide the "e" cell
 		  .cellFilter(function(d){ return d.label !== "e" })
 		  .scale(colorScale);
 
@@ -133,7 +143,7 @@ export default function Timeline(args){
 		    .on('zoom', () => {
 		    	const event = d3.getEvent();
 		      	const {k, x, y} = event.transform;
-		      	self._pVersions.attr('transform', (d,i)=>`translate(${x + self._xScale(i) * k}, 0)`);
+		      	self._pVersions.attr('transform', d=>`translate(${x + self._xScale(d.i) * k}, 0)`);
 		    })
 		  
 		self._svg.call(zoom)
@@ -172,12 +182,12 @@ export default function Timeline(args){
 
 	function _load(){
 		self.ajax.getVersions(window.project_id).then(d=>{
-			self._data = d.content;
 			_setupContainer();
-			_renderProjectVersions(d.content.project_versions);
+			[self._versions, self._days] = _processVersions(d.content.project_versions);
+			_setupScales();
+			_renderProjectVersions(self._versions);
 			_fillByAuthor();
 			_setupTooltips();
-			//_setupTimeline();
 			_setupZoom();
 			console.log(d.content)
 
