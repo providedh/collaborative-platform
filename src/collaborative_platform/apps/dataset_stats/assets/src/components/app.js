0dfe3bb4b058ef5abfd2c1b08835f69b270368d8
Janchorizo <jancho@usal.es>
2020-02-28 13:51:33 +0100

    Update app component to use entities. Add dummy stats for debug.

--- src/collaborative_platform/apps/dataset_stats/assets/src/components/app.js
+++ src/collaborative_platform/apps/dataset_stats/assets/src/components/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import EntitySelector from './entitySelector';
+import ProjectStats from './projectStats';
 import Timeline from '../helpers/timeline';
 
 
@@ -13,6 +14,34 @@ export default function App(){
 	return(
 		<div>
 			<EntitySelector currentSelection={null}/>
+			<ProjectStats stats={[
+				{
+					name: 'place',
+					count: 12,
+					coverage: 30,
+					location: 'header',
+					distinct_doc_occurrences: 3,
+					document_count: 10,
+					attributes: []
+				},
+				{
+					name: 'person',
+					count: 22,
+					coverage: 80,
+					location: 'body',
+					distinct_doc_occurrences: 8,
+					document_count: 10,
+					attributes: [
+						{
+							name: 'age',
+							distinct_values: 3,
+							trend_percentage: 80,
+							trend_value: 35,
+							coverage: 30
+						}
+					]
+				}
+				]}/>
 		</div>
 	);
 }
