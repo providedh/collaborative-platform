1982b90c54e5d9bbf3fa024a6828b578caf3cf10
Janchorizo <jancho@usal.es>
2020-02-28 13:50:17 +0100

    Add project stats component.

---
+++ src/collaborative_platform/apps/dataset_stats/assets/src/components/projectStats.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import LoadingComponent from './loading';
+import EntityStats from './entityStats';
+
+export default function({stats}){
+	let content = ''
+	if(stats == null){
+		content = <LoadingComponent />;
+	}else{
+		content = stats.map((entity,i)=><EntityStats key={i} data={entity}/>);
+	}
+
+	return(
+		<div className="box">
+	        <div className="box__header">
+	          	<h3 className="box__text--title">Entity and attribute coverage of the dataset</h3>
+	        </div>
+	        <div id="stats" className="box__content">
+	          	{content}
+	        </div>
+      	</div>
+	);
+}
