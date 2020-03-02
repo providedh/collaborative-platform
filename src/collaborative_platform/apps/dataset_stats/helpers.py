99bbda80ecd2fd879c791bee9fcd10ecaed807b5
Janchorizo <jancho@usal.es>
2020-03-02 13:20:32 +0100

    Refurbish stats endpoint code. It now retrieves the stats corresponding to the project version selected.

--- src/collaborative_platform/apps/dataset_stats/helpers.py
+++ src/collaborative_platform/apps/dataset_stats/helpers.py
@@ -27,7 +27,8 @@ def get_project_versions_files(project_id):
         prev_version = data[idx-1]
         
         for filename in entry['pv_files'].keys():
-            if not(filename in prev_version['pv_files']) or not(prev_version['pv_files'][filename]['version'] == entry['pv_files'][filename]['version']):
+            if not(filename in prev_version['pv_files']) or \
+                    not(prev_version['pv_files'][filename]['version'] == entry['pv_files'][filename]['version']):
                 entry['files'][filename] = entry['pv_files'][filename]
     
     for entry in data:
@@ -35,6 +36,26 @@ def get_project_versions_files(project_id):
         entry['files'] = tuple(entry['files'].values())
 
     return data
+
+def files_for_project_version(project: str, version: float)->Iterable[FileVersion]:
+    [file_version_counter, commit_counter] = str(version).split('.')
+
+    project_version = ProjectVersion.objects.get(
+        project=project, 
+        file_version_counter=file_version_counter,
+        commit_counter=commit_counter)
+
+    clean_xml = lambda xml: xml[len('<?xml version="1.0"?>'):] if xml.startswith('<?xml version="1.0"?>') else xml
+    get_content = lambda fv: clean_xml(fv.get_content())
+
+    file_contents = tuple((get_content(fv), fv.file.name) for fv in project_version.file_versions.all())
+
+    #files_in_project_version = FileVersion.objects\
+    #    .filter(file__in=project_version.project.files)\
+    #    .filter(creation_date__le=project_version.date)\
+    #    .distinct('file')
+
+    return file_contents        
 
 def create_summary_for_document(doc_raw: str, doc_name:str='undefined')->pd.DataFrame: 
     doc_tree = et.fromstring(doc_raw.encode())
@@ -90,19 +111,19 @@ def get_stats(stats_df):
             .attr_value \
             .value_counts()
     
-    stats = ({
-        'name': row[0],
-        'short_name': row[0].split('}')[1] if '}' in row[0] else row[0],
-        'count': row[1]['tag_id']['unique'],
-        'coverage': round(100*row[1]['document']['unique'] / n_docs, 2),
-        'n_docs': row[1]['document']['unique'],
+    stats = tuple({
+        #'name': row[0],
+        'name': row[0].split('}')[1] if '}' in row[0] else row[0],
+        'count': int(row[1]['tag_id']['unique']),
+        'coverage': float(round(100*row[1]['document']['unique'] / n_docs, 2)),
+        'distinct_doc_occurrences': int(row[1]['document']['unique']),
         'location': row[1]['location']['top'],
         'attributes': tuple({
                 'name': attr[0].split('}')[1] if '}' in attr[0] else attr[0],
-                'top_perc': round(round(100*attr[1]['attr_value']['freq']/attr[1]['attr_value']['count']),2),
-                'top_value': attr[1]['attr_value']['top'],
-                'distinct_values': attr_value_counts[row[0]][attr[0]].count(),
-                'coverage': round(100*attr[1]['document']['count']/row[1]['tag_id']['unique'], 2),
+                'trend_percentage': float(round(round(100*attr[1]['attr_value']['freq']/attr[1]['attr_value']['count']),2)),
+                'trend_value': str(attr[1]['attr_value']['top']),
+                'distinct_values': int(attr_value_counts[row[0]][attr[0]].count()),
+                'coverage': float(round(100*attr[1]['document']['count']/row[1]['tag_id']['unique'], 2)),
                 'values_json': json.dumps(list(attr_value_counts[row[0]][attr[0]].head().items()))
             }for attr in tag_g.get_group(row[0]).groupby('attr_name').describe().iterrows())
     } for row in tag_stats.iterrows())
--- src/collaborative_platform/apps/dataset_stats/views.py
+++ src/collaborative_platform/apps/dataset_stats/views.py
@@ -14,37 +14,12 @@ from . import helpers
 @user_has_access()
 def main(request, project_id=1):  # type: (HttpRequest, int, int) -> HttpResponse
     project = Project.objects.get(pk=project_id)
-    files_query = Q(project=project)
-    files = File.objects.filter(files_query)
-
-    get_latest_content = lambda f: f.versions \
-        .latest('creation_date') \
-        .get_content() \
-        .split('<?xml version="1.0"?>')[1] \
-        .strip()
-
     return render(request, "dataset_stats/app.html", {
         "title":project.title, 
         "project_id":project_id,
         "DEVELOPMENT":True,
-        "tags": [],
-        "document_count": len(files),
     })
         
-    file_gen = ((get_latest_content(file), file.name) for file in files)
-    stats_df = helpers.create_summary_for_document_collection(file_gen)
-    tags = helpers.get_stats(stats_df)
-
-    content = {
-    	"title":project.title, 
-    	"project_id":project_id,
-    	"DEVELOPMENT":True,
-    	"tags": tags,
-    	"document_count": len(files),
-    }
-
-    return render(request, "dataset_stats/app.html", content)
-
 @login_required
 @user_has_access()
 def versions(request, project_id=1):  # type: (HttpRequest, int, float) -> JSONResponse
@@ -58,36 +33,11 @@ def stats(request, project_id, project_version):  # type: (HttpRequest, int, flo
     data = {}
 
     try:
-        version_num = float(project_version)
-        #versions = ProjectVersion.objects.filter()
-        data = {'version': version_num, 'entities': [
-            {
-                'name': 'place',
-                'count': 12,
-                'coverage': 30,
-                'location': 'header',
-                'distinct_doc_occurrences': 3,
-                'document_count': 10,
-                'attributes': []
-            },
-            {
-                'name': 'person',
-                'count': 22,
-                'coverage': 80,
-                'location': 'body',
-                'distinct_doc_occurrences': 8,
-                'document_count': 10,
-                'attributes': [
-                    {
-                        'name': 'age',
-                        'distinct_values': 3,
-                        'trend_percentage': 80,
-                        'trend_value': 35,
-                        'coverage': 30
-                    }
-                ]
-            }
-            ]}
+        files = helpers.files_for_project_version(project_id, project_version)
+        stats_df = helpers.create_summary_for_document_collection(files)
+        stats = helpers.get_stats(stats_df)
+
+        data = {'entities': stats, 'document_count': len(files), 'version': project_version}
     except ValueError:
         data = {v: project_version, entities: []}
     
