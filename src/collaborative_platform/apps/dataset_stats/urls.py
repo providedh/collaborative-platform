9531a44de44e47a3783f2d5717a5268745d38e5f
Janchorizo <jancho@usal.es>
2020-03-02 11:36:30 +0100

    Remove landing page from dataset stats app.

--- src/collaborative_platform/apps/dataset_stats/urls.py
+++ src/collaborative_platform/apps/dataset_stats/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'dataset_stats'
 
 urlpatterns = [
-	path('project/<int:project_id>/landing', views.landing, name='landing'),
 	path('project/<int:project_id>/versions/', views.versions, name='versions'),
 	path('project/<int:project_id>/version/<str:project_version>/stats/', views.stats, name='stats'),
     path('project/<int:project_id>/', views.main, name='main'),
