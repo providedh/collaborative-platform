aaff473accf64a3c6fcf41ef4386b5f5c6d7bd74
Janchorizo <jancho@usal.es>
2020-03-02 10:04:27 +0100

    Add body section to entity details.

--- src/collaborative_platform/apps/dataset_stats/assets/src/components/entityStats/entityStats.js
+++ src/collaborative_platform/apps/dataset_stats/assets/src/components/entityStats/entityStats.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 
 import Header from './header';
+import Body from './body';
 
 export default ({data})=>{
   const [expanded, setExpanded] = useState(false);
 
-	return(<li className="list-group-item">
-    <ul className="list-group entity rounded {expanded===true?'expanded':''}">
-      <Header data={data} onClick={()=>setExpanded(!expanded)}/>
-    </ul>
-  </li>);
+	return(
+		<li className="list-group-item">
+		    <ul className="list-group entity rounded {expanded===true?'expanded':''}">
+		      <Header data={data} onClick={()=>setExpanded(!expanded)}/>
+		      <Body data={data}/>
+		    </ul>
+		</li>);
 }
