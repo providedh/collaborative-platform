6094586ede92e55a885c38684698e0ca38c9f9b3
Janchorizo <jancho@usal.es>
2020-03-02 16:09:53 +0100

    Add code for the entity selector.

--- src/collaborative_platform/apps/dataset_stats/assets/src/components/entitySelector.js
+++ src/collaborative_platform/apps/dataset_stats/assets/src/components/entitySelector.js
@@ -1,13 +1,58 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
 
 import LoadingComponent from './loading';
 
-export default function({currentSelection}){
-	let content = ''
-	if(currentSelection == null){
+const entityPreset = ['person', 'place', 'date'];
+
+function useEntities(stats){
+	const [entities, setEntities] = useState([]);
+
+	useEffect(()=>{
+		const newEntities = stats.map(e=>[e.name, e.long_name, entityPreset.includes(e.name)]);
+		setEntities(newEntities);
+	},[stats]);
+
+	const toggleEntity = (e)=>{
+		const newEntities = entities.map(([name, long_name, checked])=>[name, long_name, name!=e?checked:!checked]);
+		setEntities(newEntities);
+	}
+
+	return [entities, toggleEntity];
+}
+
+function useDisplayChecks(entities){
+	useEffect(()=>{
+		for(let [entity, long_name, shouldShow] of entities){
+			const node = document.getElementById(long_name);
+
+			if(shouldShow) node.classList.remove('d-none');
+			else node.classList.add('d-none');
+		}
+	},[entities]);
+}
+
+export default function({stats}){
+	const [entities, toggleEntity] = useEntities(stats);
+	useDisplayChecks(entities);
+
+	let content = '';
+
+	if(entities.length == 0){
 		content = <LoadingComponent />;
 	}else{
-		// create inputs
+
+		content = entities.map(([entity, long_name, checked])=>(
+			<div key={long_name} className="form-check form-check-inline">
+				<input className="form-check-input" 
+					type="checkbox" 
+					checked={checked}
+					id={entity + 'check'} 
+					onChange={()=>toggleEntity(entity)}/>
+				<label className="form-check-label" htmlFor={entity + 'check'}>
+			    	{entity[0].toUpperCase() + entity.slice(1).toLowerCase()}
+			  	</label>
+			</div>));
 	}
 
 	return(
