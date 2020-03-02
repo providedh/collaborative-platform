760d72821776e99397d43e322c23c8aac8f0edb9
Janchorizo <jancho@usal.es>
2020-03-02 10:04:16 +0100

    Add code for entity stats expanded body section.

--- src/collaborative_platform/apps/dataset_stats/assets/src/components/entityStats/body.js
+++ src/collaborative_platform/apps/dataset_stats/assets/src/components/entityStats/body.js
@@ -1,13 +1,9 @@
 import React from 'react';
-export default ()=>(
-	<div className="d-flex justify-content-center">
-	    <div className="spinner-grow text-primary" role="status">
-	      <span className="sr-only">Loading...</span>
-	    </div>
-	    <div className="spinner-grow text-primary" role="status">
-	      <span className="sr-only">Loading...</span>
-	    </div>
-	    <div className="spinner-grow text-primary" role="status">
-	      <span className="sr-only">Loading...</span>
-	    </div>
-    </div>);
+
+import EntityInfo from './entityInfo';
+import AttributeList from './attributeList';
+
+export default ({data})=>[
+	<EntityInfo key="ei" data={data}/>, 
+	<AttributeList key="al" data={data}/>,
+	];
