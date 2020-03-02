6655a45775a904c37c267af7d55002e701aa8f64
Janchorizo <jancho@usal.es>
2020-03-02 16:10:10 +0100

    Add id attribute to rendered entity statistics.

--- src/collaborative_platform/apps/dataset_stats/assets/src/components/entityStats/entityStats.js
+++ src/collaborative_platform/apps/dataset_stats/assets/src/components/entityStats/entityStats.js
@@ -5,9 +5,9 @@ import Body from './body';
 
 export default ({data})=>{
   const [expanded, setExpanded] = useState(false);
-
+  	
 	return(
-		<li className="list-group-item">
+		<li id={data.long_name} className="list-group-item">
 		    <ul className={`list-group entity rounded ${expanded===true?'expanded':''}`}>
 		      <Header data={data} onClick={()=>setExpanded(!expanded)}/>
 		      <Body data={data}/>
