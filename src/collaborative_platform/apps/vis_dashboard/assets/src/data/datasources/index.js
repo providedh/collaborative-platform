788a8416529949942c50c1bdc9cc9ee908f7f466
Janchorizo <jancho@usal.es>
2020-03-03 12:05:01 +0100

    Export document contents data source.

--- src/collaborative_platform/apps/vis_dashboard/assets/src/data/datasources/index.js
+++ src/collaborative_platform/apps/vis_dashboard/assets/src/data/datasources/index.js
@@ -1,6 +1,8 @@
 import DataSource from './datasource.js';
 import EntityDataSource from './entity_datasource.js';
+import DocumentDataSource from './document_datasource.js';
 export {
 	DataSource,
-	EntityDataSource
+	EntityDataSource,
+	DocumentDataSource,
 };
