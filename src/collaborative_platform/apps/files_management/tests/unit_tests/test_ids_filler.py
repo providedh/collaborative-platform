c046eb886348443a68d9c551d7c9cbf5a12aa58e
bug-rancher <bug-rancher@protonmail.com>
2020-03-05 12:10:07 +0100

    Add unit test for correcting custom entities in IDsFiller

--- src/collaborative_platform/apps/files_management/tests/unit_tests/test_ids_filler.py
+++ src/collaborative_platform/apps/files_management/tests/unit_tests/test_ids_filler.py
@@ -47,6 +47,26 @@ class TestIDsFiller:
 
         assert result_xml == expected_xml
 
+    def test_correct_ids__correct_custom_entities__string(self, monkeypatch):
+        monkeypatch.setattr(IDsFiller, 'get_entities_schemes_from_db', fake_get_entities_schemes_from_db)
+        monkeypatch.setattr(IDsFiller, 'correct_listable_entities_ids', do_nothing)
+        monkeypatch.setattr(IDsFiller, 'correct_unlistable_entities_ids', do_nothing)
+
+        source_file_path = os.path.join(DIRNAME, 'test_files', 'source_files', 'ids_filler_test_file.xml')
+        expected_file_path = os.path.join(DIRNAME, 'test_files', 'expected_files',
+                                          'correct_ids__correct_custom_entities.xml')
+
+        source_xml = read_file(source_file_path)
+        expected_xml = read_file(expected_file_path)
+
+        project_id = 1
+
+        ids_filler = IDsFiller()
+
+        result_xml = ids_filler.correct_ids(source_xml, project_id)
+
+        assert result_xml == expected_xml
+
 
 def read_file(path):
     with open(path, 'r') as file:
