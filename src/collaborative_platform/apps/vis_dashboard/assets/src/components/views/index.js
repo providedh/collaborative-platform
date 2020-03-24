373ebe7b58c83ec77e083f32bc8f364d6b400668
Janchorizo <jancho@usal.es>
2020-03-24 12:44:11 +0100

    Remove unimplemented views from the available options.

--- src/collaborative_platform/apps/vis_dashboard/assets/src/components/views/index.js
+++ src/collaborative_platform/apps/vis_dashboard/assets/src/components/views/index.js
@@ -1,15 +1,13 @@
 import PixelCorpus from './PixelCorpus';
-import Dummy from './Dummy';
-import GeoMap from './GeoMap';
+//import Dummy from './Dummy';
+//import GeoMap from './GeoMap';
 import Histogram from './Histogram';
 import Heatmap from './Heatmap';
 import DotMatrix from './DotMatrix';
 import DocumentView from './DocumentView';
 
 export default {
-    'Dummy': Dummy,
     'PixelCorpus': PixelCorpus, 
-    'Map': GeoMap,
 	'Histogram': Histogram,
 	'Heatmap': Heatmap,
     'DotMatrix': DotMatrix,
