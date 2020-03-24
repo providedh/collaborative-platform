80e0e90ca6525f297505ae5cf82015956bdf8463
Janchorizo <jancho@usal.es>
2020-03-24 16:36:25 +0100

    Add optional label for select control.

--- src/collaborative_platform/apps/vis_dashboard/assets/src/components/ui/FormControls/SelectionControl/SelectionControl.js
+++ src/collaborative_platform/apps/vis_dashboard/assets/src/components/ui/FormControls/SelectionControl/SelectionControl.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from './style.module.css';
 
-export default function SelectionControl({name, value='', onValueChange, params={options: []}}) {
+export default function SelectionControl({name, value='', onValueChange, params={options: [], labels: null}}) {
     const handleChange = e=>onValueChange(e.target.value);
+    const labels = params.labels==null?params.options:params.labels;
 
     function createLabel(name){
         const separated = name.replace(/[A-Z]/g, x=>' '+x),
@@ -15,7 +16,7 @@ export default function SelectionControl({name, value='', onValueChange, params=
             <div className={"form-group "+styles.formGroupOverride}>
                 <label htmlFor={'input-'+name}>{createLabel(name)}</label>
                 <select className="form-control" value={value} onChange={handleChange}>
-                    {params.options.map((o, i) => <option value={o} key={i}>{o}</option>)}
+                    {params.options.map((o, i) => <option value={o} key={i}>{createLabel(labels[i])}</option>)}
                 </select>
             </div>
         </div>
