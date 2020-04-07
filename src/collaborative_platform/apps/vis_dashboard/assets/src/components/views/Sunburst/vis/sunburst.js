00a260e3991cb36562132f146fb0a540cfca2d4b
Janchorizo <jancho@usal.es>
2020-04-07 10:03:31 +0200

    Add color schemes.

--- src/collaborative_platform/apps/vis_dashboard/assets/src/components/views/Sunburst/vis/sunburst.js
+++ src/collaborative_platform/apps/vis_dashboard/assets/src/components/views/Sunburst/vis/sunburst.js
@@ -24,6 +24,33 @@ export default function Sunburst(){
 		return (value)=>self[key]=value;
 	}
 
+	function setupColorSchemes(){
+		self._colorSchemes = {
+			author: d3.scaleOrdinal().range(d3.schemePaired),
+			document: d3.scaleOrdinal().range(d3.schemePaired),
+			category: d3.scaleOrdinal()
+				.range(Object.values(settings.taxonomy).map(x=>x.color))
+				.domain(Object.values(settings.taxonomy).map(x=>x.name)),
+			attribute: d3.scaleOrdinal().range(d3.schemePaired),
+			degree: d3.interpolateYlOrRd,
+			certainty: d3.scaleOrdinal()
+				.range(d3.schemeYlOrRd(5))
+				.domain(['unknown', 'very low', 'low', 'medium', 'high', 'very high'])
+		}
+	}
+
+	function renderLegend(width, height, levels, containerRef){
+
+	}
+
+	function setupHoverTooltip(){
+
+	}
+
+	function renderSunburst(width, height, data, numLevels, levels, containerRef){
+
+	}
+
 	function _render(width, height, data, numLevels, levels, containerRef){
 		console.log(width, height, data, numLevels, levels, containerRef)
 	}
