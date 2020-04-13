d309bd5e73ec6170c4168f953976bf5eb4e34353
Janchorizo <jancho@usal.es>
2020-04-13 15:58:33 +0200

    Update to ES6 Array and Object syntax.

--- src/collaborative_platform/apps/vis_dashboard/assets/src/components/ui/Form/Form.js
+++ src/collaborative_platform/apps/vis_dashboard/assets/src/components/ui/Form/Form.js
@@ -4,12 +4,14 @@ import styles from './style.module.css';
 import FormControls from '../FormControls';
 
 export default function Form({options ,onUpdate}) {
-    const name2index = {};
-    options.forEach((o,i) => name2index[o.name]=i);
+    const name2index = Object.fromEntries(
+        options.map((o,i) => [o.name, i])
+    );
 
     function handleChange(name, value) {
-        const newOptions = Array.from(options);
+        const newOptions = [...options];
         newOptions[name2index[name]].value = value;
+
         onUpdate(newOptions);
     }
 
