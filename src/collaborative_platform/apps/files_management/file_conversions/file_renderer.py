81cc2d69a70854faf14d3acc9a33a61739bd28d6
bug-rancher <bug-rancher@protonmail.com>
2020-04-17 11:16:33 +0200

    Add appending custom entities if FileRenderer class

--- src/collaborative_platform/apps/files_management/file_conversions/file_renderer.py
+++ src/collaborative_platform/apps/files_management/file_conversions/file_renderer.py
@@ -7,7 +7,7 @@ from apps.files_management.file_conversions.xml_tools import get_first_xpath_mat
 from apps.api_vis.models import Entity, EntityVersion, EntityProperty
 from apps.projects.models import EntitySchema
 
-from collaborative_platform.settings import XML_NAMESPACES, DEFAULT_ENTITIES, NS_MAP
+from collaborative_platform.settings import XML_NAMESPACES, DEFAULT_ENTITIES, NS_MAP, CUSTOM_ENTITY
 
 
 class FileRenderer:
@@ -26,6 +26,7 @@ class FileRenderer:
 
         self.__create_tree()
         self.__append_listable_entities()
+        self.__append_custom_entities()
 
 
 
@@ -101,6 +102,20 @@ class FileRenderer:
 
             self.__append_elements_to_the_list(elements, list_xpath)
 
+    def __append_custom_entities(self):
+        for entity in self.__custom_entities:
+            entities_versions = self.__get_entities_versions_from_db(entity.name)
+            elements = self.__create_entities_elements(entities_versions, custom=True)
+
+            if entity.body_list:
+                list_xpath = f'/default:TEI/default:text/default:body/default:div[@type="{entity.name}"]/' \
+                             f'default:listObject[@type="{entity.name}List"]'
+            else:
+                list_xpath = f'/default:TEI/default:teiHeader/default:sourceDesc/' \
+                             f'default:listObject[@type="{entity.name}List"]'
+
+            self.__append_elements_to_the_list(elements, list_xpath)
+
     def __get_entities_versions_from_db(self, entity_type):
         entities_versions = EntityVersion.objects.filter(
             file_version=self.__file_version,
@@ -109,35 +124,45 @@ class FileRenderer:
 
         return entities_versions
 
-    def __create_entities_elements(self, entities_versions):
+    def __create_entities_elements(self, entities_versions, custom=False):
         elements = []
 
         for entity_version in entities_versions:
-            entity_element = self.__create_entity_element(entity_version)
+            entity_element = self.__create_entity_element(entity_version, custom)
 
-            self.__append_entity_properties(entity_element, entity_version)
+            self.__append_entity_properties(entity_element, entity_version, custom)
 
             elements.append(entity_element)
 
         return elements
 
-    def __create_entity_element(self, entity_version):
+    def __create_entity_element(self, entity_version, custom=False):
         default_prefix = '{%s}' % XML_NAMESPACES['default']
         xml_prefix = '{%s}' % XML_NAMESPACES['xml']
 
-        entity_element = etree.Element(default_prefix + entity_version.entity.type, nsmap=NS_MAP)
+        if not custom:
+            entity_element = etree.Element(default_prefix + entity_version.entity.type, nsmap=NS_MAP)
+        else:
+            entity_element = etree.Element(default_prefix + 'object', nsmap=NS_MAP)
+            entity_element.set('type', entity_version.entity.type)
+
         entity_element.set(xml_prefix + 'id', entity_version.entity.xml_id)
         entity_element.set('resp', str(entity_version.entity.created_by_id))
 
         return entity_element
 
-    def __append_entity_properties(self, entity_element, entity_version):
+    def __append_entity_properties(self, entity_element, entity_version, custom=False):
         entities_properties = EntityProperty.objects.filter(
             entity_version=entity_version
         )
 
+        if not custom:
+            properties = DEFAULT_ENTITIES[entity_version.entity.type]['properties']
+        else:
+            properties = CUSTOM_ENTITY['properties']
+
         for entity_property in entities_properties:
-            xpath = DEFAULT_ENTITIES[entity_version.entity.type]['properties'][entity_property.name]['xpath']
+            xpath = properties[entity_property.name]['xpath']
             value = entity_property.get_value(as_str=True)
 
             add_property(entity_element, xpath, value)
