6926cf81d673c63e60b18d983c33cf3ccf358709
Janchorizo <jancho@usal.es>
2020-04-20 11:18:58 +0200

    Update the bar chart component to use the new data hook.

--- src/collaborative_platform/apps/vis_dashboard/assets/src/components/views/Histogram/Histogram.js
+++ src/collaborative_platform/apps/vis_dashboard/assets/src/components/views/Histogram/Histogram.js
@@ -2,65 +2,11 @@ import React, { useEffect, useState, useRef } from "react";
 
 import styles from './style.module.css';
 import css from './style.css';
-import dummy_data from './dummy_data';
-import dummy_overlay from './overlay_data';
 import render from './render';
 import getConfig from './config';
+import useData from './data';
 import {DataClient, useCleanup} from '../../../data';
 
-function useData(dataClient, dimension){
-	const [data, setData] = useState(null);
-
-	useEffect(()=>{
-        dataClient.clearSubscriptions();
-        if(dimension == 'Number of entities per type'){
-            dataClient.subscribe('entity', data=>{
-                const count = {};
-                data.filtered.forEach(x=>count.hasOwnProperty(x.type)?count[x.type]++:count[x.type]=1);
-                const entries = Object.entries(count).map(x=>({type:x[0], count:x[1]}));
-                setData(entries);
-            });
-        }else if(dimension == 'Most common entities'){
-            dataClient.subscribe('entity', data=>{
-                const count = {};
-                data.filtered.forEach(x=>count.hasOwnProperty(x.name)?count[x.name]++:count[x.name]=1);
-                const entries = Object
-                    .entries(count)
-                    .map(x=>({name:x[0], count:x[1]}))
-                    .sort((x,y)=>x.count-y.count);
-                const mostCommon = [];
-
-                for(let i=0; i<entries.length && i<5; i++)
-                    mostCommon.push(entries.pop());
-                if(entries.length > 0)
-                    mostCommon.push({name:'other', count:entries.reduce((ac,dc)=>ac+dc.count, 0)});
-
-                setData(mostCommon);
-            });
-        }else if(dimension == 'Number of entities per document'){
-            dataClient.subscribe('entity', data=>{
-                const count = {};
-                data.filtered.forEach(x=>count.hasOwnProperty(x.file_name)?count[x.file_name]++:count[x.file_name]=1);
-                const entries = Object.entries(count).map(x=>({file_name:x[0], count:x[1]}));
-                setData(entries);
-            });
-        }else{
-		    setData(dummy_data[dimension])
-        }
-	}, [dimension])
-
-	return data;
-}
-
-function useOverlay(renderOverlay, dimension, overlay){
-	const [overlayData, setOverlayData] = useState(dummy_overlay[overlay][dimension]);
-
-	useEffect(()=>{
-		setOverlayData(renderOverlay===true?dummy_overlay[overlay][dimension]:null)
-	}, [renderOverlay, dimension, overlay])
-
-	return overlayData;
-}
 
 function getOnEventCallback(dataClient, dimension, barDirection){
     return (event)=>{
@@ -68,7 +14,7 @@ function getOnEventCallback(dataClient, dimension, barDirection){
     };
 }
 
-function Histogram({dimension, barDirection, renderOverlay, overlay, layout}) {
+function Histogram({layout, dimension, barDirection}) {
 	const [refContainer, refCanvas, refOverlayCanvas] = [useRef(), useRef(), useRef()];
 	const [width, height] = layout!=undefined?[layout.w, layout.h]:[4,4];
 
@@ -76,7 +22,8 @@ function Histogram({dimension, barDirection, renderOverlay, overlay, layout}) {
     useCleanup(dataClient);
 	const data = useData(dataClient, dimension);
     const onEvent = getOnEventCallback(dataClient, dimension, barDirection);
-	//const overlay_data = useOverlay(renderOverlay, dimension, overlay);
+
+    console.log(dimension, barDirection, data)
 
     useEffect(()=>render(
     		refContainer.current, 
@@ -84,7 +31,7 @@ function Histogram({dimension, barDirection, renderOverlay, overlay, layout}) {
     		refOverlayCanvas.current, 
     		data, 
     		null, 
-    		renderOverlay, 
+    		false, 
     		barDirection,
             onEvent), // Render 
     	[width, height, barDirection, data]); // Conditions
