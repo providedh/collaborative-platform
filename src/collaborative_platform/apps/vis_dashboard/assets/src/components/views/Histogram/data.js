0c3d205de883bef6b564e41884cf332c7f729de7
Janchorizo <jancho@usal.es>
2020-04-20 22:18:09 +0200

    Add newer use cases.

--- src/collaborative_platform/apps/vis_dashboard/assets/src/components/views/Histogram/data.js
+++ src/collaborative_platform/apps/vis_dashboard/assets/src/components/views/Histogram/data.js
@@ -33,7 +33,7 @@ function entitiesPerDoc(dataClient, setData){
 			filterDimension: 'fileId',
 			all: d3Array.group(all, x=>x.file_id),
 			filtered: d3Array.group(filtered, x=>x.file_id)
-		})
+		});
 	});
 }
 
@@ -44,36 +44,53 @@ function entitiesPerType(dataClient, setData){
 			filterDimension: 'entityType',
 			all: d3Array.group(all, x=>x.type),
 			filtered: d3Array.group(filtered, x=>x.type)
-		})
+		});
 	});	
 }
 
 function commonEntities(dataClient, setData){
 	return 0;
 	dataClient.subscribe('certainty', ({all, filtered})=>{
-		setData({
-			dimension: 'type',
-			all: d3Array.group(all, x=>x.file_id),
-			filtered: d3Array.group(filtered, x=>x.file_id)
-		})
-	});	
+		alert('The entity type of the annotated piece is not yet exposed');
+		//setData({
+		//	dimension: 'type',
+		//	filterDimension: 'fileId',
+		//	all: d3Array.group(all, x=>x.file),
+		//	filtered: d3Array.group(filtered, x=>x.file)
+		//});
+	});
 }
 
 function annotationsPerDoc(dataClient, setData){
 	dataClient.subscribe('certainty', ({all, filtered})=>{
-		console.log(all)
+		setData({
+			dimension: 'file id',
+			filterDimension: 'fileId',
+			all: d3Array.group(all, x=>x.file),
+			filtered: d3Array.group(filtered, x=>x.file)
+		});
 	});
 }
 
 function annotationsPerCategory(dataClient, setData){
 	dataClient.subscribe('certainty', ({all, filtered})=>{
-
+		setData({
+			dimension: 'category',
+			filterDimension: 'certaintyCategory',
+			all: d3Array.group(all, x=>x.category.sort().join()),
+			filtered: d3Array.group(filtered, x=>x.category.sort().join())
+		});
 	});
 }
 
 function annotationsPerAuthor(dataClient, setData){
 	dataClient.subscribe('certainty', ({all, filtered})=>{
-
+		setData({
+			dimension: 'author',
+			filterDimension: 'author',
+			all: d3Array.group(all, x=>x.resp),
+			filtered: d3Array.group(filtered, x=>x.resp)
+		});
 	});
 }
 
@@ -85,6 +102,12 @@ function commonAttributeValues(dataClient, setData){
 
 function annotationAttributes(dataClient, setData){
 	dataClient.subscribe('certainty', ({all, filtered})=>{
-
+		console.log(all, filtered)
+		setData({
+			dimension: 'attribute',
+			filterDimension: 'certaintyMatch',
+			all: d3Array.group(all, x=>x.match),
+			filtered: d3Array.group(filtered, x=>x.match)
+		});
 	});
 }
