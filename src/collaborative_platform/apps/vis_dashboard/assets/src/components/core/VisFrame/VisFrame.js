6b6569e55e2ba45d8e2636522923c31296e2dd75
Janchorizo <jancho@usal.es>
2020-04-23 19:32:13 +0200

    Apply JavaScript standard style guide to ViewDetailsPanel, VisFrame, Workspace.

--- src/collaborative_platform/apps/vis_dashboard/assets/src/components/core/VisFrame/VisFrame.js
+++ src/collaborative_platform/apps/vis_dashboard/assets/src/components/core/VisFrame/VisFrame.js
@@ -1,31 +1,41 @@
-import React from 'react';
-import styles from './style.module.css';
-import css from './style.css';
-import {WithAppContext} from 'app_context';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function VisFrame({index, focused, onViewFocus, onViewClose, children}) {
-    const getTitlePadding = title=>({'paddingLeft': 'calc(50% - '+((children.props.dimension.length/2)*5.5)+'px)'});
-    
-    return(
-        <div className={`border ${styles.frame} ${focused?'focused border-primary':''}`}>
-            <div className={styles.topbar}>
-                {children.props.hasOwnProperty('dimension')?
-                <span style={getTitlePadding(children.props.dimension)}>
-                    {children.props.dimension}
-                </span>
-                :''}
-                <button onClick={()=>onViewClose(index)} type="button" className="close" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-                <button onClick={()=>onViewFocus(index)} type="button" className="close" aria-label="Close">
-                  <span aria-hidden="true">&#9881;</span>
-                </button>
-            </div>
-            <div className={styles.container}>
-                <WithAppContext>
-                    {children}
-                </WithAppContext>
-            </div>
-        </div>
-    )
+import styles from './style.module.css'
+import css from './style.css'
+import { WithAppContext } from 'app_context'
+
+export default function VisFrame ({ index, focused, onViewFocus, onViewClose, children }) {
+  const getTitlePadding = title => ({ paddingLeft: 'calc(50% - ' + ((children.props.dimension.length / 2) * 5.5) + 'px)' })
+
+  return (
+    <div className={`border ${styles.frame} ${focused === true ? 'focused border-primary' : ''}`}>
+      <div className={styles.topbar}>
+        {Object.hasOwnProperty.call(children.props, 'dimension')
+          ? <span style={getTitlePadding(children.props.dimension)}>
+            {children.props.dimension}
+          </span>
+          : ''}
+        <button onClick={() => onViewClose(index)} type="button" className="close" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <button onClick={() => onViewFocus(index)} type="button" className="close" aria-label="Close">
+          <span aria-hidden="true">&#9881;</span>
+        </button>
+      </div>
+      <div className={styles.container}>
+        <WithAppContext>
+          {children}
+        </WithAppContext>
+      </div>
+    </div>
+  )
+}
+
+VisFrame.propTypes = {
+  index: PropTypes.number,
+  focused: PropTypes.bool,
+  onViewFocus: PropTypes.func,
+  onViewClose: PropTypes.func,
+  children: PropTypes.element
 }
