833a2c3a7084fd148ca8bdbb0bdeeca385b1c01d
bug-rancher <bug-rancher@protonmail.com>
2020-06-02 13:17:33 +0200

    Add discarding adding reference to entity, when entity exist and entity is unlistable. Add test for discarding adding reference to entity, when entity exist and entity is unlistable

--- src/collaborative_platform/apps/close_reading/tests/integration_tests/test_annotator_with_ws_and_db.py
+++ src/collaborative_platform/apps/close_reading/tests/integration_tests/test_annotator_with_ws_and_db.py
@@ -425,6 +425,9 @@ class TestAnnotatorWithWsAndDb5:
         response = await communicator.receive_json_from()
         verify_response(test_name, response, request_nr)
 
+        date_entities_in_db = Entity.objects.filter(type='date')
+        assert len(date_entities_in_db) == 2
+
         request = {
             'method': 'modify',
             'payload': [
@@ -441,6 +444,9 @@ class TestAnnotatorWithWsAndDb5:
         await communicator.send_json_to(request)
         response = await communicator.receive_json_from()
         verify_response(test_name, response, request_nr)
+
+        date_entities_in_db = Entity.objects.filter(type='date')
+        assert len(date_entities_in_db) == 2
 
         await communicator.disconnect()
 
@@ -1881,6 +1887,76 @@ class TestAnnotatorWithWsAndDb34:
         await communicator.send_json_to(request)
         response = await communicator.receive_json_from()
         verify_response(test_name, response, request_nr)
+
+        await communicator.disconnect()
+
+
+@pytest.mark.usefixtures('annotator_with_ws_and_db_setup', 'reset_db_files_directory_before_each_test')
+@pytest.mark.asyncio
+@pytest.mark.django_db()
+@pytest.mark.integration_tests
+class TestAnnotatorWithWsAndDb35:
+    async def test_discard_adding_reference_to_entity_to_text__entity_exist__entity_unlistable(self):
+        test_name = inspect.currentframe().f_code.co_name
+
+        project_id = 1
+        file_id = 1
+        user_id = 2
+
+        communicator = get_communicator(project_id, file_id, user_id)
+
+        await communicator.connect()
+        await communicator.receive_json_from()
+
+        request = {
+            'method': 'modify',
+            'payload': [
+                {
+                    'method': 'POST',
+                    'element_type': 'tag',
+                    'parameters': {
+                        'start_pos': 265,
+                        'end_pos': 271,
+                    }
+                }
+            ]
+        }
+
+        await communicator.send_json_to(request)
+        _ = await communicator.receive_json_from()
+
+        request = {
+            'method': 'modify',
+            'payload': [
+                {
+                    'method': 'POST',
+                    'element_type': 'reference',
+                    'edited_element_id': 'ab-1',
+                    'new_element_id': 'date-0'
+                }
+            ]
+        }
+        request_nr = 0
+
+        await communicator.send_json_to(request)
+        response = await communicator.receive_json_from()
+        verify_response(test_name, response, request_nr)
+
+        date_entities_in_db = Entity.objects.filter(type='date')
+        assert len(date_entities_in_db) == 2
+
+        request = {
+            'method': 'discard',
+            'payload': [2]
+        }
+        request_nr = 1
+
+        await communicator.send_json_to(request)
+        response = await communicator.receive_json_from()
+        verify_response(test_name, response, request_nr)
+
+        date_entities_in_db = Entity.objects.filter(type='date')
+        assert len(date_entities_in_db) == 2
 
         await communicator.disconnect()
 
