f97f6b3d6c34d23e1696d1acab583ed4a2f48ce7
bug-rancher <bug-rancher@protonmail.com>
2020-06-16 10:09:00 +0200

    Add accepting adding reference to entity, when entity exist and entity is unlistable. Add test for accepting adding reference to entity, when entity exist and entity is unlistable

--- src/collaborative_platform/apps/close_reading/tests/integration_tests/test_annotator_with_ws_and_db.py
+++ src/collaborative_platform/apps/close_reading/tests/integration_tests/test_annotator_with_ws_and_db.py
@@ -3184,6 +3184,76 @@ class TestAnnotatorWithWsAndDb56:
         await communicator.disconnect()
 
 
+@pytest.mark.usefixtures('annotator_with_ws_and_db_setup', 'reset_db_files_directory_before_each_test')
+@pytest.mark.asyncio
+@pytest.mark.django_db()
+@pytest.mark.integration_tests
+class TestAnnotatorWithWsAndDb57:
+    async def test_accept_adding_reference_to_entity_to_text__entity_exist__entity_unlistable(self):
+        test_name = inspect.currentframe().f_code.co_name
+
+        project_id = 1
+        file_id = 1
+        user_id = 2
+
+        communicator = get_communicator(project_id, file_id, user_id)
+
+        await communicator.connect()
+        await communicator.receive_json_from()
+
+        request = {
+            'method': 'modify',
+            'payload': [
+                {
+                    'method': 'POST',
+                    'element_type': 'tag',
+                    'parameters': {
+                        'start_pos': 265,
+                        'end_pos': 271,
+                    }
+                }
+            ]
+        }
+
+        await communicator.send_json_to(request)
+        _ = await communicator.receive_json_from()
+
+        request = {
+            'method': 'modify',
+            'payload': [
+                {
+                    'method': 'POST',
+                    'element_type': 'reference',
+                    'edited_element_id': 'ab-1',
+                    'new_element_id': 'date-0'
+                }
+            ]
+        }
+        request_nr = 0
+
+        await communicator.send_json_to(request)
+        response = await communicator.receive_json_from()
+        verify_response(test_name, response, request_nr)
+
+        date_entities_in_db = Entity.objects.filter(type='date')
+        assert len(date_entities_in_db) == 2
+
+        request = {
+            'method': 'save',
+            'payload': [1, 2]
+        }
+        request_nr = 1
+
+        await communicator.send_json_to(request)
+        response = await communicator.receive_json_from()
+        verify_response(test_name, response, request_nr)
+
+        date_entities_in_db = Entity.objects.filter(type='date')
+        assert len(date_entities_in_db) == 2
+
+        await communicator.disconnect()
+
+
 def get_communicator(project_id, file_id, user_id=None):
     communicator = WebsocketCommunicator(
         application=application,
