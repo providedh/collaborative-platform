7fdfea2b13183c588acbc011c413083940f582c0
janchorizo <jancho@usal.es>
2020-06-22 13:10:06 +0200

    Change to using websockets implementation.

--- src/collaborative_platform/apps/close_reading/assets/src/common/helpers/websocket_api/index.js
+++ src/collaborative_platform/apps/close_reading/assets/src/common/helpers/websocket_api/index.js
@@ -1,4 +1,4 @@
 import socket from './websocket.js'
 import requests from './request.js'
 import mockup from './mockup.js'
-export default {socket: mockup, requests}
+export default {socket, requests}
