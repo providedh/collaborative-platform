27efcf627cb520f1c4e89700e63dece69e5f92dd
janchorizo <jancho@usal.es>
2020-06-23 10:32:34 +0200

    Add TEI entity types.

--- src/collaborative_platform/apps/close_reading/assets/src/common/types/index.js
+++ src/collaborative_platform/apps/close_reading/assets/src/common/types/index.js
@@ -1,2 +1,3 @@
 export {Selection, SelectionType} from './selection.js'
 export {AtomicAction, AtomicActionType, AtomicActionTarget, AtomicActionObject} from './atomic_operations.js'
+export {default as TEIentities} from './tei_entity.js'
