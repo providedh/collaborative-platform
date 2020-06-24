74e0b14044264eb4a09af8c71f9f117196186b67
janchorizo <jancho@usal.es>
2020-06-24 09:58:48 +0200

    Restructure inline annotation form.

--- src/collaborative_platform/apps/close_reading/assets/src/components/entityPanel/annotate_form.js
+++ src/collaborative_platform/apps/close_reading/assets/src/components/entityPanel/annotate_form.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 
 import {TEIentities} from 'common/types'
 import {WithAppContext} from 'common/context/app'
@@ -23,16 +23,25 @@ function Form (props) {
   const propertyOptions = entityType.properties.map(x => 
     <option key={x} value={x}>{x}</option>)
 
-  const defState = {
-    locus: 'name',
-    certainty: 'medium',
-    categories: [],
+  const defState = Object.assign({}, {
+    locus: 'value',
+    cert: 'medium',
+    ana: [],
     assertedValue: '',
-    attribute: '',
-    description: ''
-  }
+    match: '',
+    desc: ''
+  }, props.annotation === undefined ? {} : {
+    ...props.annotation,
+    ana: props.annotation.ana
+      .split(' ')
+      .map(x => x.split('#')[1])
+  })
 
   const [form, update] = useState(defState)
+  useEffect(() => {
+    update(defState)
+  }, [Object.hasOwnProperty.call(props, 'annotation')])
+
 
   function handleUpdate (key, value) {
     const newForm = Object.assign({}, form)
@@ -42,7 +51,7 @@ function Form (props) {
       if (value === 'name') {
         newForm.assertedValue = props.entity.type
       } else if (value === 'attribute') {
-        newForm.attribute = entityType.properties[0]
+        newForm.match = entityType.properties[0]
       }
     }
 
@@ -51,93 +60,100 @@ function Form (props) {
 
   let assertedValueInput = <div className="col">
     <label htmlFor="assertedValue">Value</label>
-    <input className="form-control form-control-sm" 
-           type="text"
-           value={form.assertedValue}
-           onChange={e => handleUpdate('assertedValue', e.target.value)}
-           id="assertedValue"/>
+    <input className="form-control form-control-sm"
+      type="text"
+      value={form.assertedValue}
+      onChange={e => handleUpdate('assertedValue', e.target.value)}
+      id="assertedValue"/>
   </div>
 
-  if (form.locus === 'name'){
+  if (form.locus === 'name') {
     assertedValueInput = <div className="col">
       <div className="form-group">
         <label htmlFor="assertedValue">Asserted value</label>
-        <select className="form-control form-control-sm" 
-                value={form.assertedValue}
-                onChange={e => handleUpdate('assertedValue', e.target.value)}
-                id="assertedValue">
+        <select className="form-control form-control-sm"
+          value={form.assertedValue}
+          onChange={e => handleUpdate('assertedValue', e.target.value)}
+          id="assertedValue">
           {entityOptions}
         </select>
       </div>
     </div>
   }
 
-  return <form className="p-2 rounded" style={{backgroundColor: 'rgba(0,0,0,.03)'}}>
+  return <form className="p-2 rounded" style={{ backgroundColor: 'rgba(0,0,0,.03)' }}>
     <div className="row">
       <div className="col">
-          <div className="form-group">
-            <label htmlFor="locus">Target</label>
-            <select className="form-control form-control-sm" 
-                    id="locus"
-                    value={form.locus}
-                    onChange={e => handleUpdate('locus', e.target.value)}>
-              <option value="name">Tag name</option>
-              <option value="value">Value</option>
-              <option value="attribute">Attribute</option>
-            </select>
-          </div>
-        </div>
-        <div className="col">
-          <div className="form-group">
-            <label htmlFor="certainty">Certainty level</label>
-            <select className="form-control form-control-sm" 
-                    id="certainty"
-                    value={form.certainty}
-                    onChange={e => handleUpdate('certainty', e.target.value)}>
-              <option value="very low">Very low</option>
-              <option value="low">Low</option>
-              <option value="medium">Medium</option>
-              <option value="high">High</option>
-              <option value="very high">Very high</option>
-            </select>
-          </div>
-        </div>
-        <div className="col">
-          <div className="form-group">
-            <label htmlFor="category">Categories</label>
-            <select multiple 
-                    className="form-control form-control-sm" 
-                    id="category"
-                    onChange={e => handleUpdate('categories', [...e.target.selectedOptions].map(x => x.value))}>
-              {categoryOptions}
-            </select>
-          </div>
-        </div>          
-      </div>
-      <div className="row">
-        {assertedValueInput}
-        <div className={"col " + (form.locus==='attribute'?'':'d-none')}>
-          <div className="form-group">
-            <label htmlFor="attributeName">Attribute</label>
-            <select className="form-control form-control-sm" 
-                    value={form.attribute}
-                    onChange={e => handleUpdate('attribute', e.target.value)}
-                    id="attributeName">
-              {propertyOptions}
-            </select>
-          </div>
+        <div className="form-group">
+          <label htmlFor="locus">Target</label>
+          <select className="form-control form-control-sm"
+            id="locus"
+            value={form.locus}
+            onChange={e => handleUpdate('locus', e.target.value)}>
+            <option value="name">Tag name</option>
+            <option value="value">Value</option>
+            <option value="attribute">Attribute</option>
+          </select>
         </div>
       </div>
-      <div className="row">
-        <div className="col">
-          <label htmlFor="description">Description</label>
-          <input className="form-control form-control-sm" type="text" id="description"/>
+      <div className="col">
+        <div className="form-group">
+          <label htmlFor="certainty">Certainty level</label>
+          <select className="form-control form-control-sm"
+            id="certainty"
+            value={form.cert}
+            onChange={e => handleUpdate('cert', e.target.value)}>
+            <option value="very low">Very low</option>
+            <option value="low">Low</option>
+            <option value="medium">Medium</option>
+            <option value="high">High</option>
+            <option value="very high">Very high</option>
+          </select>
         </div>
       </div>
-      <div className="row mt-2">
-        <div className="col">
-        <button type="button" className="btn btn-outline-primary">Create annotation</button>
+      <div className="col">
+        <div className="form-group">
+          <label htmlFor="category">Categories</label>
+          <select multiple
+            className="form-control form-control-sm"
+            id="category"
+            value={form.ana}
+            onChange={e => handleUpdate('ana', [...e.target.selectedOptions].map(x => x.value))}>
+            {categoryOptions}
+          </select>
         </div>
       </div>
+    </div>
+    <div className="row">
+      {assertedValueInput}
+      <div className={'col ' + (form.locus === 'attribute' ? '' : 'd-none')}>
+        <div className="form-group">
+          <label htmlFor="attributeName">Attribute</label>
+          <select className="form-control form-control-sm"
+            value={form.match}
+            onChange={e => handleUpdate('match', e.target.value)}
+            id="attributeName">
+            {propertyOptions}
+          </select>
+        </div>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <label htmlFor="description">Description</label>
+        <input className="form-control form-control-sm"
+          type="text"
+          onChange={e => handleUpdate('desc', e.target.value)}
+          value={form.desc}
+          id="description"/>
+      </div>
+    </div>
+    <div className="row mt-2">
+      <div className="col">
+        <button type="button"
+          onClick={() => props.callback(form)}
+          className="btn btn-sm btn-outline-primary">{props.submitText}</button>
+      </div>
+    </div>
   </form>
 }
