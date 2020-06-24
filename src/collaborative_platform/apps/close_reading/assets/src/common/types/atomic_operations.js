2a894cb9ae98273c6e8774717cd3279c6cf1b9c9
janchorizo <jancho@usal.es>
2020-06-24 09:54:51 +0200

    Refactor atomic operations.

--- src/collaborative_platform/apps/close_reading/assets/src/common/types/atomic_operations.js
+++ src/collaborative_platform/apps/close_reading/assets/src/common/types/atomic_operations.js
@@ -1,109 +1,109 @@
 
-export const AtomicActionType = {add: 'add', modify:'modify', delete: 'delete'}
-export const AtomicActionTarget = {text: 'text', entity: 'entity', certainty: 'certainty'}
-export const AtomicActionObject = {
-    tag: 'tag', 
-    referece: 'reference', 
-    property: 'property', 
-    unification: 'unification',
-    certainty: 'certainty'
+export const ActionType = { add: 'add', modify: 'modify', delete: 'delete' }
+export const ActionTarget = { text: 'text', entity: 'entity', certainty: 'certainty' }
+export const ActionObject = {
+  tag: 'tag',
+  referece: 'reference',
+  property: 'property',
+  unification: 'unification',
+  certainty: 'certainty'
 }
 
-export function AtomicAction (type, target, object){
-    const action = actions?.[type]?.[target]?.[object]
-    return action
+export function AtomicActionBuilder (target, type, object) {
+  const action = actions?.[target]?.[type]?.[object]
+  return action
 }
 
 const actions = {
-    text: {
-        add:{
-            tag: (start, end)=>({
-                "element_type": "tag",
-                "method": "POST",
-                "parameters": {
-                    "start_pos": start,
-                    "end_pos": end
-                }
-            })
-        },
-        modify: {
-            tag: (id, oldStart, oldEnd, newStart, newEnd)=>({
-                "element_type": "tag",
-                "method": "PUT",
-                "edited_element_id": id,
-                "parameters": {
-                    "start_pos": oldStart,
-                    "end_pos": oldEnd,
-                    "new_start_pos": newStart,
-                    "new_end_pos": newEnd
-                }
-            })
-        },
-        delete: {
-            tag: (id)=>({
-                "element_type": "tag",
-                "method": "DELETE",
-                "edited_element_id": id
-            })
+  text: {
+    add: {
+      tag: (start, end) => ({
+        element_type: 'tag',
+        method: 'POST',
+        parameters: {
+          start_pos: start,
+          end_pos: end
         }
+      })
     },
-    entity: {
-        add:{
-            reference: ()=>({}),
-            property: (id, properties)=>({
-                "element_type": "entity_property",
-                "method": "POST",
-                "edited_element_id": id,
-                "parameters": properties
-            }),
-            unification: ()=>({})
-        },
-        modify: {
-            reference: ()=>({}),
-            property: ()=>({}),
-            unification: ()=>({})
-        },
-        delete: {
-            reference: ()=>({}),
-            property: (id, property)=>({
-                "element_type": "entity_property",
-                "method": "DELETE",
-                "edited_element_id": id,
-                "old_element_id": property
-            }),
-            unification: ()=>({})
+    modify: {
+      tag: (id, oldStart, oldEnd, newStart, newEnd) => ({
+        element_type: 'tag',
+        method: 'PUT',
+        edited_element_id: id,
+        parameters: {
+          start_pos: oldStart,
+          end_pos: oldEnd,
+          new_start_pos: newStart,
+          new_end_pos: newEnd
         }
+      })
     },
-    certainty: {
-        add:{
-            certainty: (target, locus, categories, certainty, assertedValue, description)=>({
-                "element_type": "certainty",
-                "method": "POST",
-                "new_element_id": target,
-                "parameters": {
-                    "categories": categories,
-                    "locus": "value",
-                    "certainty": certainty,
-                    "asserted_value": assertedValue,
-                    "description": description
-                }
-            })
-        },
-        modify: {
-            certainty: (id, attributeName, attributeValue)=>({
-                "element_type": "certainty",
-                "method": "PUT",
-                "edited_element_id": id,
-                "old_element_id": attributeName,
-                "parameters": {[attributeName]: attributeValue}
-            })
-        },
-        delete: {
-            certainty: (id)=>({
-                "element_type": "certainty",
-                "method": "DELETE",
-                "edited_element_id": id
-            })
-        }
+    delete: {
+      tag: (id) => ({
+        element_type: 'tag',
+        method: 'DELETE',
+        edited_element_id: id
+      })
     }
+  },
+  entity: {
+    add: {
+      reference: () => ({}),
+      property: (id, properties) => ({
+        element_type: 'entity_property',
+        method: 'POST',
+        edited_element_id: id,
+        parameters: properties
+      }),
+      unification: () => ({})
+    },
+    modify: {
+      reference: () => ({}),
+      property: () => ({}),
+      unification: () => ({})
+    },
+    delete: {
+      reference: () => ({}),
+      property: (id, property) => ({
+        element_type: 'entity_property',
+        method: 'DELETE',
+        edited_element_id: id,
+        old_element_id: property
+      }),
+      unification: () => ({})
+    }
+  },
+  certainty: {
+    add: {
+      certainty: (target, locus, categories, certainty, assertedValue, description) => ({
+        element_type: 'certainty',
+        method: 'POST',
+        new_element_id: target,
+        parameters: {
+          categories: categories,
+          locus: locus,
+          certainty: certainty,
+          asserted_value: assertedValue,
+          description: description
+        }
+      })
+    },
+    modify: {
+      certainty: (id, attributeName, attributeValue) => ({
+        element_type: 'certainty',
+        method: 'PUT',
+        edited_element_id: id,
+        old_element_id: attributeName,
+        parameters: { [attributeName]: attributeValue }
+      })
+    },
+    delete: {
+      certainty: (id) => ({
+        element_type: 'certainty',
+        method: 'DELETE',
+        edited_element_id: id
+      })
+    }
+  }
 }
--- src/collaborative_platform/apps/close_reading/assets/src/common/types/index.js
+++ src/collaborative_platform/apps/close_reading/assets/src/common/types/index.js
@@ -1,4 +1,4 @@
 export {Selection, SelectionType} from './selection.js'
-export {AtomicAction, AtomicActionType, AtomicActionTarget, AtomicActionObject} from './atomic_operations.js'
+export {AtomicActionBuilder, ActionType, ActionTarget, ActionObject} from './atomic_operations.js'
 export {default as TEIentities} from './tei_entity.js'
 export {WebsocketRequest, WebsocketRequestType} from './websocket_request.js'
