fcabbab51a40955f0d0c79eb204fce7c9f23c738
janchorizo <jancho@usal.es>
2020-06-24 09:43:25 +0200

    Add minimal Websocket request type.

--- src/collaborative_platform/apps/close_reading/assets/src/common/types/index.js
+++ src/collaborative_platform/apps/close_reading/assets/src/common/types/index.js
@@ -1,3 +1,4 @@
 export {Selection, SelectionType} from './selection.js'
 export {AtomicAction, AtomicActionType, AtomicActionTarget, AtomicActionObject} from './atomic_operations.js'
 export {default as TEIentities} from './tei_entity.js'
+export {WebsocketRequest, WebsocketRequestType} from './websocket_request.js'
---
+++ src/collaborative_platform/apps/close_reading/assets/src/common/types/websocket_request.js
@@ -0,0 +1,14 @@
+export const WebsocketRequestType = {
+  modify: 'modify',
+  discard: 'discard',
+  save: 'save'
+}
+
+export function WebsocketRequest(type, payload) {
+  const request = {
+    method: type,
+    payload: payload
+  }
+  
+  return alert(JSON.stringify(action))
+}
