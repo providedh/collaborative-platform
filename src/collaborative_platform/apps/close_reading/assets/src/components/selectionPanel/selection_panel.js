9e42607701271d652680c505471413338bd2e2a7
janchorizo <jancho@usal.es>
2020-06-24 10:19:18 +0200

    Refactor selection panel.

--- src/collaborative_platform/apps/close_reading/assets/src/components/selectionPanel/selection_panel.js
+++ src/collaborative_platform/apps/close_reading/assets/src/components/selectionPanel/selection_panel.js
@@ -1,58 +1,73 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-import {AnnotationCreationPanel} from 'components/annotationCreationPanel'
-import {EntityCreationPanel} from 'components/entityCreationPanel'
+import { ActionType, ActionTarget, ActionObject, AtomicActionBuilder } from 'common/types'
+import { AnnotationCreationPanel } from 'components/annotationCreationPanel'
+import { EntityCreationPanel } from 'components/entityCreationPanel'
 import styles from './selection_panel.module.css'
 
-const CreationOption = {entity: 'Add entity', certainty: 'Annotate uncertainty'}
+const CreationOption = { entity: 'Add entity', certainty: 'Annotate uncertainty' }
 
 function Navigation (option, setOption) {
   const navigation = (option === null
     ? (<div className={styles.navigation}>
-        <button type="button" 
-                className="btn btn-primary" 
-                onClick={()=>setOption(CreationOption.entity)}>
+      <button type="button"
+        className="btn btn-sm btn-primary"
+        onClick={() => setOption(CreationOption.entity)}>
           Add entity
-        </button>
-        <button type="button" 
-                className="btn btn-primary ml-3"
-                onClick={()=>setOption(CreationOption.certainty)}>
+      </button>
+      <button type="button"
+        className="btn btn-sm btn-primary ml-3"
+        onClick={() => setOption(CreationOption.certainty)}>
           Annotate uncertainty
-        </button>
-        </div>)
+      </button>
+    </div>)
     : (<div className={styles.navigation}>
-        <button type="button" 
-                className="btn btn-link"
-                onClick={()=>setOption(null)}>
+      <button type="button"
+        className="btn btn-link btn-sm"
+        onClick={() => setOption(null)}>
           &lt; Back
-        </button>
-        <button disabled type="button" className="btn btn-light">{option}</button>
-        <hr className="m-0"/>
-        </div>)
+      </button>
+      <button disabled type="button" className="btn btn-sm btn-light">{option}</button>
+      <hr className="m-0"/>
+    </div>)
   )
   return navigation
+}
+
+function onTagCreate (selection, entity, attributes) {
+  const tagBuilder = AtomicActionBuilder(ActionTarget.text, ActionType.add, ActionObject.tag)
+  const attributeBuilder = AtomicActionBuilder(ActionTarget.entity, ActionType.add, ActionObject.property)
+
+  const tagAction = tagBuilder(...selection.target)
+  const attributeAction = attributeBuilder('TEMP', attributes)
+  const actions = [tagAction, attributeAction]
+  alert(JSON.stringify(actions))
 }
 
 export default function SelectionPanel (props) {
   const [option, setOption] = useState(null)
 
-  let body = '';
+  let body = ''
   if (option !== null) {
     body = (option === CreationOption.certainty
       ? <AnnotationCreationPanel />
-      : <EntityCreationPanel />)
+      : <EntityCreationPanel callback={(entity, attributes) => onTagCreate(props.selection, entity, attributes)}/>)
   }
 
-  let bodyCssClasses = ['card-body']
-  if (body === '') {bodyCssClasses.push('d-none')}
+  const bodyCssClasses = ['card-body']
+  if (body === '') { bodyCssClasses.push('d-none') }
 
   return <div className={styles.selectionPanel + ' card'}>
     <div className="card-header">
-      {Navigation(option, setOption )}
+      {Navigation(option, setOption)}
     </div>
     <div className={bodyCssClasses.join(' ')}>
       {body}
     </div>
   </div>
+}
+
+SelectionPanel.propTypes = {
+  selection: PropTypes.object
 }
