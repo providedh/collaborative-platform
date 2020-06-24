a0219c708b67bdec610dcf23ea18150a12f4a68e
janchorizo <jancho@usal.es>
2020-06-24 09:56:31 +0200

    Add entity create button.

--- src/collaborative_platform/apps/close_reading/assets/src/components/entityCreationPanel/entity_creation_panel.js
+++ src/collaborative_platform/apps/close_reading/assets/src/components/entityCreationPanel/entity_creation_panel.js
@@ -36,7 +36,7 @@ function EntityCreationPanel (props) {
   const attributeFields = Object.entries(attributes).map(([name, value]) => <div className="form-group col-4" key={name}>
     <label htmlFor={name}>{name}</label>
     <input type="text" 
-           className="form-control" 
+           className="form-control form-control-sm" 
            id={name} 
            value={value} 
            onChange={e => setAttributes(Object.assign({}, attributes, {[name]: e.target.value}))} />
@@ -49,12 +49,24 @@ function EntityCreationPanel (props) {
     <form>
       <div className="form-group">
         <label htmlFor="entityType">Entity type</label>
-        <select className="form-control" id="entityType" value={entity} onChange={e => handleEntityChange(e.target.value)}>
+        <select className="form-control form-control-sm" id="entityType" value={entity} onChange={e => handleEntityChange(e.target.value)}>
           {entityOptions}
         </select>
       </div>
       <div className="row">
         {attributeFields}
+        <div className="form-group col-4">
+          <button className="btn btn-link btn-sm"
+            type="button"
+            aria-controls="collapse"></button>
+        </div>
+      </div>
+      <div className="row">
+        <div className="form-group col-4">
+          <button className="btn btn-outline-primary btn-sm"
+            onClick={() => props.callback(entity, attributes)}
+            type="button">Create</button>
+        </div>
       </div>
     </form>
   </div>
