ba7b0655905002152170d7d48457e7470ec5d1a8
bug-rancher <bug-rancher@protonmail.com>
2020-06-24 14:12:58 +0200

    Update database fixture for api_vis tests

--- src/collaborative_platform/apps/api_vis/tests/integration_tests/test_api_vis_with_db.py
+++ src/collaborative_platform/apps/api_vis/tests/integration_tests/test_api_vis_with_db.py
@@ -21,36 +21,36 @@ class TestApiVisWithDb:
         client.force_login(user)
 
         cliques = Clique.objects.all()
-        assert cliques.count() == 0
+        assert cliques.count() == 1
 
         url = f'/api/vis/projects/{project_id}/cliques/'
 
         payload = {
-            'name': 'first_clique',
-            'entities': [11, 15],
+            'name': 'second_clique',
+            'entities': [12, 15],
             'certainty': 'high',
-            'project_version': 6.0,
+            'project_version': 6.1,
         }
 
         response = client.post(url, payload, content_type="application/json")
 
         assert response.status_code == 200
 
-        cliques = Clique.objects.all()
-        assert cliques.count() == 1
+        cliques = Clique.objects.all().order_by('-id')
+        assert cliques.count() == 2
 
         clique = cliques[0]
-        assert clique.asserted_name == 'first_clique'
+        assert clique.asserted_name == 'second_clique'
         assert clique.unifications.count() == 2
 
         response_content = json.loads(response.content)
 
         expected_response = {
-            'name': 'first_clique',
-            'id': 1,
+            'name': 'second_clique',
+            'id': 2,
             'unification_statuses': [
                 {
-                    'id': 11,
+                    'id': 12,
                     'status': 200,
                     'message': 'OK',
                 },
@@ -73,7 +73,7 @@ class TestApiVisWithDb:
         client.force_login(user)
 
         cliques = Clique.objects.all()
-        assert cliques.count() == 0
+        assert cliques.count() == 1
 
         url = f'/api/vis/projects/{project_id}/cliques/'
 
@@ -81,7 +81,7 @@ class TestApiVisWithDb:
             'entities': [
                 {
                     'file_path': 'Project_1/group_0_long_annotated_0_modified_xml',
-                    'xml_id': 'ingredient-0',
+                    'xml_id': 'ingredient-1',
                 },
                 {
                     'file_path': 'Project_1/group_0_long_annotated_1_modified_xml',
@@ -89,29 +89,28 @@ class TestApiVisWithDb:
                 },
             ],
             'certainty': 'high',
-            'project_version': 6.0,
+            'project_version': 6.1,
         }
 
         response = client.post(url, payload, content_type="application/json")
 
         assert response.status_code == 200
 
-        cliques = Clique.objects.all()
-        assert cliques.count() == 1
+        cliques = Clique.objects.all().order_by('-id')
+        assert cliques.count() == 2
 
         clique = cliques[0]
-        assert clique.asserted_name == 'Rag'
         assert clique.unifications.count() == 2
 
         response_content = json.loads(response.content)
 
         expected_response = {
-            'name': 'Rag',
-            'id': 1,
+            'name': 'Ingwer',
+            'id': 2,
             'unification_statuses': [
                 {
                     'file_path': 'Project_1/group_0_long_annotated_0_modified_xml',
-                    'xml_id': 'ingredient-0',
+                    'xml_id': 'ingredient-1',
                     'status': 200,
                     'message': 'OK',
                 },
@@ -134,29 +133,14 @@ class TestApiVisWithDb:
         user = User.objects.get(id=user_id)
         client.force_login(user)
 
-        cliques = Clique.objects.all()
-        assert cliques.count() == 0
-
-        url = f'/api/vis/projects/{project_id}/cliques/'
-
-        payload = {
-            'name': 'first_clique',
-            'entities': [11, 15],
-            'certainty': 'high',
-            'project_version': 6.0,
-        }
-
-        response = client.post(url, payload, content_type="application/json")
-        response_content = json.loads(response.content)
-
-        clique_id = response_content['id']
+        clique_id = 1
 
         url = f'/api/vis/projects/{project_id}/cliques/{clique_id}/entities/'
 
         payload = {
-            'entities': [17],
+            'entities': [16],
             'certainty': 'medium',
-            'project_version': 6.0,
+            'project_version': 6.1,
         }
 
         response = client.put(url, payload, content_type="application/json")
@@ -174,7 +158,7 @@ class TestApiVisWithDb:
         expected_response = {
             'unification_statuses': [
                 {
-                    'id': 17,
+                    'id': 16,
                     'status': 200,
                     'message': 'OK',
                 },
@@ -191,22 +175,7 @@ class TestApiVisWithDb:
         user = User.objects.get(id=user_id)
         client.force_login(user)
 
-        cliques = Clique.objects.all()
-        assert cliques.count() == 0
-
-        url = f'/api/vis/projects/{project_id}/cliques/'
-
-        payload = {
-            'name': 'first_clique',
-            'entities': [11, 15],
-            'certainty': 'high',
-            'project_version': 6.0,
-        }
-
-        response = client.post(url, payload, content_type="application/json")
-        response_content = json.loads(response.content)
-
-        clique_id = response_content['id']
+        clique_id = 1
 
         url = f'/api/vis/projects/{project_id}/cliques/{clique_id}/entities/'
 
@@ -214,11 +183,11 @@ class TestApiVisWithDb:
             'entities': [
                 {
                     'file_path': 'Project_1/group_0_long_annotated_2_modified_xml',
-                    'xml_id': 'ingredient-1',
+                    'xml_id': 'ingredient-0',
                 },
             ],
             'certainty': 'medium',
-            'project_version': 6.0,
+            'project_version': 6.1,
         }
 
         response = client.put(url, payload, content_type="application/json")
@@ -237,7 +206,7 @@ class TestApiVisWithDb:
             'unification_statuses': [
                 {
                     'file_path': 'Project_1/group_0_long_annotated_2_modified_xml',
-                    'xml_id': 'ingredient-1',
+                    'xml_id': 'ingredient-0',
                     'status': 200,
                     'message': 'OK',
                 },
@@ -255,53 +224,18 @@ class TestApiVisWithDb:
         client.force_login(user)
 
         cliques = Clique.objects.all()
-        assert cliques.count() == 0
+        assert cliques.count() == 1
 
         url = f'/api/vis/projects/{project_id}/cliques/'
 
         payload = {
             'name': 'first_clique',
-            'entities': [11, 15],
+            'entities': [12, 15],
             'certainty': 'high',
-            'project_version': 6.0,
+            'project_version': 6.1,
         }
 
         client.post(url, payload, content_type="application/json")
-
-        commits = Commit.objects.all()
-        assert commits.count() == 0
-
-        project = Project.objects.get(
-            id=project_id
-        )
-
-        last_project_version = project.versions.order_by('-id')[0]
-        assert last_project_version.file_version_counter == 6
-        assert last_project_version.commit_counter == 0
-
-        cliques = Clique.objects.all()
-        assert cliques.count() == 1
-
-        cliques_committed = Clique.objects.filter(
-            created_in_commit__isnull=False
-        )
-
-        assert cliques_committed.count() == 0
-
-        unifications_committed = Clique.objects.filter(
-            created_in_commit__isnull=False
-        )
-
-        assert unifications_committed.count() == 0
-
-        url = f'/api/vis/projects/{project_id}/commits/'
-
-        payload = {
-            'message': 'First commit message'
-        }
-
-        response = client.post(url, payload, content_type="application/json")
-        assert response.status_code == 200
 
         commits = Commit.objects.all()
         assert commits.count() == 1
@@ -315,7 +249,7 @@ class TestApiVisWithDb:
         assert last_project_version.commit_counter == 1
 
         cliques = Clique.objects.all()
-        assert cliques.count() == 1
+        assert cliques.count() == 2
 
         cliques_committed = Clique.objects.filter(
             created_in_commit__isnull=False
@@ -328,3 +262,38 @@ class TestApiVisWithDb:
         )
 
         assert unifications_committed.count() == 2
+
+        url = f'/api/vis/projects/{project_id}/commits/'
+
+        payload = {
+            'message': 'First commit message'
+        }
+
+        response = client.post(url, payload, content_type="application/json")
+        assert response.status_code == 200
+
+        commits = Commit.objects.all()
+        assert commits.count() == 2
+
+        project = Project.objects.get(
+            id=project_id
+        )
+
+        last_project_version = project.versions.order_by('-id')[0]
+        assert last_project_version.file_version_counter == 6
+        assert last_project_version.commit_counter == 2
+
+        cliques = Clique.objects.all()
+        assert cliques.count() == 2
+
+        cliques_committed = Clique.objects.filter(
+            created_in_commit__isnull=False
+        )
+
+        assert cliques_committed.count() == 2
+
+        unifications_committed = Unification.objects.filter(
+            created_in_commit__isnull=False
+        )
+
+        assert unifications_committed.count() == 4
