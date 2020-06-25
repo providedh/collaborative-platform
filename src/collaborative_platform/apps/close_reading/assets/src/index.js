838c80456ca4bdec40bbc82a6964558969f82383
janchorizo <jancho@usal.es>
2020-06-25 11:23:30 +0200

    Lint main code.

--- src/collaborative_platform/apps/close_reading/assets/src/index.js
+++ src/collaborative_platform/apps/close_reading/assets/src/index.js
@@ -1,10 +1,9 @@
-import React from "react";
-import { render } from "react-dom";
+import React from 'react'
+import { render } from 'react-dom'
 
 import { App } from 'components/app'
-import defaultTaxonomy from './default_taxonomy.js'
 
-const app_config = {
+const appConfig = {
   projectId: window.project_id,
   user: 'annotator-1',
   fileId: window.file_id,
@@ -13,4 +12,4 @@ const app_config = {
   configuration: window.preferences
 }
 
-render(<App {...app_config}/>, document.getElementById('react-root'))
+render(<App {...appConfig}/>, document.getElementById('react-root'))
