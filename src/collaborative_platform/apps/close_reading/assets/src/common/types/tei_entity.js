13dabc55fe830d203db2501dd61b5139aa981b73
janchorizo <jancho@usal.es>
2020-06-26 09:12:26 +0200

    Reimplement TEIentity as an object proxy.

--- src/collaborative_platform/apps/close_reading/assets/src/common/types/tei_entity.js
+++ src/collaborative_platform/apps/close_reading/assets/src/common/types/tei_entity.js
@@ -1,44 +1,28 @@
-export default {
-  date: {
-    properties: ['ref']
-  },
-  event: {
-    properties: ['ref']
-  },
-  object: {
-    properties: ['ref']
-  },
-  org: {
-    properties: ['ref']
-  },
-  person: {
-    properties: [
-      'forename',
-      'surname',
-      'occupation',
-      'sex',
-      'birth',
-      'death',
-      'age',
-      'ref'
-    ]
-  },
-  place: {
-    properties: ['country', 'geolocation', 'ref']
-  },
-  time: {
-    properties: ['ref']
-  },
-  ingredient: {
-    properties: ['ref']
-  },
-  productionMethod: {
-    properties: ['ref']
-  },
-  utensil: {
-    properties: ['ref']
-  },
-  default: {
-    properties: ['ref']
+function proxied () {
+  const _common = ['ref']
+  let _specific = {}
+
+  const isPrivate = (key) => key[0] === '_'
+  function update (properties) { _specific = properties }
+
+  const handler = {
+    get (target, key) {
+      if (isPrivate(key)) { throw new Error(`Attempted to access ${key}`) }
+      if (key === 'update') { return update }
+
+      if (!Object.hasOwnProperty.call(_specific, key)) {
+        return { properties: _common }
+      } else {
+        const properties = new Set([..._common, ..._specific[key].properties])
+        return { properties: [...properties] }
+      }
+    },
+    set (target, key, value) {
+      return false
+    }
   }
+
+  return new Proxy({}, handler)
 }
+
+export default proxied()
