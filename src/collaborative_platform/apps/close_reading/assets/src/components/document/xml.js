6b8615758467efc58f591306cdb8e5c900b68bba
janchorizo <jancho@usal.es>
2020-06-29 09:24:21 +0200

    Remove old XML utilities module.

--- src/collaborative_platform/apps/close_reading/assets/src/components/document/xml.js
+++
@@ -1,8 +0,0 @@
-const spacer = 'xxxx'
-
-export default {
-  spacer,
-  originalId: id => id.slice(spacer.length),
-  replacedId: xmlid => spacer + xmlid,
-  replaceXmlid: s => s.replace(/xml:id="/gm, 'id="' + spacer)
-}
