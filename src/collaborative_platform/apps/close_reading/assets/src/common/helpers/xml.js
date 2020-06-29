b4205c4a479ad86c902076220c337c48bd68518c
janchorizo <jancho@usal.es>
2020-06-29 10:13:41 +0200

    Refactor xml module. Add utility functions to expand and collapse tags.

--- src/collaborative_platform/apps/close_reading/assets/src/common/helpers/xml.js
+++ src/collaborative_platform/apps/close_reading/assets/src/common/helpers/xml.js
@@ -1,20 +1,76 @@
 const spacer = 'xxxx'
 
-export default {
-  spacer,
-  originalId: id => id.slice(spacer.length),
-  replacedId: xmlid => spacer + xmlid,
-  replaceXmlid: s => s.replace(/xml:id="/gm, 'id="' + spacer),
-  getSelfClosedTags: text => text.match(/<[^>]+\/>/gm) || [],
-  expandEmptyTag: tag => {
-      const name = tag.match(/[^ ,<,/,>]+/gm)[0]
-      const opening = tag.replace('/>','>')
-      const closing = '</'+name+'>'
-
-      return opening + closing
-  },
-  getBodyContent: content => /<body[^>]*>(?<content>(.|[\n\r])*)<\/body>/im.exec(content)?.groups?.content,
-  getBodyTagLength: content => /^(?<tag><body[^>]*>)/.exec(content).groups.tag.length
+function originalId (id) {
+  return id.slice(spacer.length)
 }
 
-// getBodyContent: content => /^<body[^>]*>(?<content>.*)<\/body>$/.exec(content)?.groups?.content,
+function replacedId (xmlid) {
+  return spacer + xmlid
+}
+
+function replaceXmlid (s) {
+  return s.replace(/xml:id="/gm, 'id="' + spacer)
+}
+
+function getSelfClosedTags (text) {
+  return text.match(/<[^>]+\/>/gm) || []
+}
+
+function expandEmptyTag (tag) {
+  const name = tag.match(/[^ ,<,/,>]+/gm)[0]
+  const opening = tag.replace('/>', '>')
+  const closing = '</' + name + '>'
+
+  return opening + closing
+}
+
+function extractAndExpandTags (original) {
+  const selfClosedTags = getSelfClosedTags(original)
+
+  return function (text) {
+    let expanded = text
+    for (const emptyTag of selfClosedTags) {
+      const expandedTag = expandEmptyTag(emptyTag)
+      expanded = expanded.replace(new RegExp(emptyTag, 'ig'), expandedTag)
+    }
+
+    return expanded
+  }
+}
+
+function extractAndCollapseTags (original) {
+  const selfClosedTags = getSelfClosedTags(original)
+
+  return function (text) {
+    let collapsed = text
+    for (const emptyTag of selfClosedTags) {
+      const expandedTag = expandEmptyTag(emptyTag)
+      collapsed = collapsed.replace(new RegExp(expandedTag, 'ig'), emptyTag)
+    }
+
+    return collapsed
+  }
+}
+
+function getBodyContent (content) {
+  const r = /<body[^>]*>(?<content>(.|[\n\r])*)<\/body>/im
+  return r.exec(content)?.groups?.content
+}
+
+function getBodyTagLength (content) {
+  const r = /^(?<tag><body[^>]*>)/
+  return r.exec(content).groups.tag.length
+}
+
+export default {
+  spacer,
+  originalId,
+  replacedId,
+  replaceXmlid,
+  getSelfClosedTags,
+  expandEmptyTag,
+  extractAndExpandTags,
+  extractAndCollapseTags,
+  getBodyContent,
+  getBodyTagLength
+}
