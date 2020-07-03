85d5e9452b16ea2f52fc3d8e53c340585b1098e2
bug-rancher <bug-rancher@protonmail.com>
2020-07-03 13:18:03 +0200

    Add test for api_vis endpoint for getting cliques which include entities from given file with start date filtering

--- src/collaborative_platform/apps/api_vis/tests/integration_tests/test_api_vis_with_db.py
+++ src/collaborative_platform/apps/api_vis/tests/integration_tests/test_api_vis_with_db.py
@@ -527,7 +527,7 @@ class TestApiVisWithDb:
         ('no_filtering', None),
         ('types', {'types': 'ingredient'}),
         ('users', {'users': '2'}),
-        # ('start_date', {'start_date': '2020-06-28T12:05:30+01:00'}),
+        ('start_date', {'start_date': '2020-06-28T12:05:30+01:00'}),
         # ('end_date', {'end_date': '2020-06-28T12:13:00+01:00'}),
         # ('date', {'date': '2020-06-28T12:15:00+01:00'}),
         # ('project_version', {'project_version': '6.3'})
