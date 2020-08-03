f4fbef3a7ca4ee7c435836db7421f4fbcfacb431
virdiignis <gbryk1@gmail.com>
2020-08-03 13:52:50 +0200

    constraints

--- src/collaborative_platform/apps/nn_disambiguator/models.py
+++ src/collaborative_platform/apps/nn_disambiguator/models.py
@@ -26,6 +26,9 @@ class Classifier(Model):
     class Meta:
         unique_together = ('project', 'entity_type')
 
+    class Meta:
+        unique_together = ('project', 'entity_type')
+
     def set_model(self, model):
         bytes_container = BytesIO()
         joblib.dump(model, bytes_container)
