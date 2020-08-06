19307b2aa391ccd293b9fb2996489e99797de918
virdiignis <gbryk1@gmail.com>
2020-08-06 11:48:55 +0200

    files textual similarity computation

--- src/collaborative_platform/apps/nn_disambiguator/data_processor.py
+++ src/collaborative_platform/apps/nn_disambiguator/data_processor.py
@@ -1,13 +1,22 @@
+import itertools
 from typing import List
+
+from sklearn.exceptions import NotFittedError
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
 
 from apps.api_vis.enums import TypeChoice
 from apps.api_vis.models import EntityVersion, EntityProperty, Entity
 from apps.nn_disambiguator import names, time, geography
-from apps.projects.models import EntitySchema
+from apps.nn_disambiguator.models import Classifier
+from apps.projects.models import EntitySchema, Project
 from collaborative_platform.settings import DEFAULT_ENTITIES
+import spacy
 
 
 class DataProcessor:
+    nlp = spacy.load('en_core_web_lg')
+
     processing_functions: {
         TypeChoice.str: {
             names.nlp_sim,
@@ -30,7 +39,7 @@ class DataProcessor:
         entity_settings = DEFAULT_ENTITIES.get(entity_type.name, None)
         self.properties = entity_settings['properties'] or {'name': {'type': TypeChoice.str}}
 
-    def calculate_similarity(self, e1: EntityVersion, e2: EntityVersion) -> List[float]:
+    def __calculate_similarity(self, e1: EntityVersion, e2: EntityVersion) -> List[float]:
         sims = []
         for property, params in self.properties.items():
             try:
@@ -51,5 +60,31 @@ class DataProcessor:
 
         return sims
 
+    def __calculate_files_text_similarity(self, e1lv, e2lv) -> float:
+        if e1lv.file_version == e2lv.file_version:
+            return 1.0
+        else:
+            nlp_file1 = self.nlp(e1lv.file_version.body_text)
+            nlp_file2 = self.nlp(e2lv.file_version.body_text)
+            return nlp_file1.similarity(nlp_file2)
+
+    def __calculate_features_vector_length(self, schema: EntitySchema) -> int:
+        v = 1  # 1 for files text similarity
+        entity = DEFAULT_ENTITIES.get(schema.name)
+        if entity is not None:
+            for property, values in entity["properties"]:
+                v += len(self.processing_functions[values["type"]])
+        else:
+            v += len(self.processing_functions[TypeChoice.str])  # only name
+
+        # 1 number for avg similarity between files of each of the other types of entities
+        v += schema.taxonomy.entities_schemas.count() - 1
+        return v
+
     def get_features_vector(self, e1: Entity, e2: Entity) -> List[float]:
-        raise NotImplementedError
+        e1lv = e1.versions.latest('id')
+        e2lv = e2.versions.latest('id')
+
+        sims = self.__calculate_similarity(e1lv, e2lv)
+
+        files_sim = self.__calculate_files_text_similarity(e1lv, e2lv)
