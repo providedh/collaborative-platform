39fa95b82eca4cddf3ac96f1c42a320977e19f4c
virdiignis <gbryk1@gmail.com>
2020-08-27 14:36:25 +0200

    names analysis tests

--- src/collaborative_platform/apps/nn_disambiguator/names.py
+++ src/collaborative_platform/apps/nn_disambiguator/names.py
@@ -6,22 +6,24 @@ import spacy
 nlp = spacy.load('en_core_web_lg')
 
 
-def ratcliff_obershelp_sim(s1: str, s2: str):
-    if s1 == s2: return 0
+def ratcliff_obershelp_sim(s1: str, s2: str) -> float:
+    if s1 is None or s2 is None: return -1.
+    if s1 == s2: return 1.
     return difflib.SequenceMatcher(None, s1, s2).ratio()
 
 
-def phonetics_sim(s1: str, s2: str):
+def phonetics_sim(s1: str, s2: str) -> int:
+    if s1 is None or s2 is None: return -1
     if s1 == s2: return 0
     return editdistance.eval(fuzzy.nysiis(s1), fuzzy.nysiis(s2))
 
 
-def levenshtein_distance(s1: str, s2: str):
+def levenshtein_distance(s1: str, s2: str) -> int:
     if s1 is None or s2 is None: return -1
     return editdistance.eval(s1, s2)
 
 
-def nlp_sim(s1: str, s2: str):
+def nlp_sim(s1: str, s2: str) -> float:
     tokens = nlp(f"{s1} {s2}")
     avsim = 0
     tokens_num_h = len(tokens) // 2
