93a3e716473b2b1b2062581f31e755e1e9dd24a5
virdiignis <gbryk1@gmail.com>
2020-08-27 16:06:42 +0200

    dates and times calculations tests

--- src/collaborative_platform/apps/nn_disambiguator/time.py
+++ src/collaborative_platform/apps/nn_disambiguator/time.py
@@ -1,10 +1,12 @@
 import re
 import sys
+from typing import Optional, Union
 from datetime import datetime
-from typing import Optional
-
 import maya
 from timefhuman import timefhuman
+
+max_seconds_apart = 24 * 60 * 60
+max_days_apart = 365 * 1000
 
 
 def to_datetime(text: str) -> Optional[datetime]:
@@ -12,7 +14,9 @@ def to_datetime(text: str) -> Optional[datetime]:
     text = re.sub("([0-9]+)_([0-9]+)", r"\1:\2", text)
 
     try:
-        dt_maya = maya.parse(text).datetime()
+        dt_maya = maya.parse(text).datetime().replace(tzinfo=None)
+        if not dt_maya:
+            raise Exception
         return dt_maya
     except:
         try:
@@ -25,31 +29,31 @@ def to_datetime(text: str) -> Optional[datetime]:
             return None
 
 
-def days_apart(d1, d2) -> int:
+def days_apart(d1: Union[datetime, str], d2: Union[datetime, str]) -> int:
     if type(d1) == str:
         d1 = to_datetime(d1)
         if d1 is None:
-            return sys.maxsize
+            return max_days_apart
     if type(d2) == str:
         d2 = to_datetime(d2)
         if d2 is None:
-            return sys.maxsize
+            return max_days_apart
 
     try:
         return abs(d1 - d2).days
     except:
-        return sys.maxsize
+        return max_days_apart
 
 
-def seconds_apart(t1, t2) -> int:
+def seconds_apart(t1: Union[datetime.time, str], t2: Union[datetime.time, str]) -> int:
     if type(t1) == str:
         t1 = to_datetime(t1)
         if t1 is None:
-            return sys.maxsize
+            return max_seconds_apart
     if type(t2) == str:
         t2 = to_datetime(t2)
         if t2 is None:
-            return sys.maxsize
+            return max_seconds_apart
 
     d = datetime.min
     d1 = d.replace(hour=t1.hour, minute=t1.minute, second=t1.second)
