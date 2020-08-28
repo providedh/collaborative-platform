220c0d78973de3fcd213f160db5d7f36ffbf1a7e
virdiignis <gbryk1@gmail.com>
2020-08-28 11:15:43 +0200

    geography tests

--- src/collaborative_platform/apps/nn_disambiguator/geography.py
+++ src/collaborative_platform/apps/nn_disambiguator/geography.py
@@ -1,15 +1,15 @@
+from typing import Tuple
+
 import geopy.distance
 from geopy import Nominatim
-
-from collaborative_platform import settings
 
 g = Nominatim(user_agent="PROVIDEDH Collaborative Platform")
 
 
-def coordinates(query: str):
+def coordinates(query: str) -> Tuple[float, float]:
     res = g.geocode(query)
     if res is None:
-        return 0, 0
+        return 0., 0.
     _, coords = res
     return coords
 
@@ -20,11 +20,3 @@ def distance(p1, p2):
 
 def names_to_distance(q1: str, q2: str):
     return distance(coordinates(q1), coordinates(q2))
-
-
-if __name__ == "__main__":
-    c1 = coordinates("Salamanca, ES")
-    c2 = coordinates("Warsaw, PL")
-    print(c1)
-    print(c2)
-    print(distance(c1, c2))
