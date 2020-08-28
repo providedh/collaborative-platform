262dc9430cbc381be8a3a79b13164c1159cfa76e
virdiignis <gbryk1@gmail.com>
2020-08-28 11:15:43 +0200

    geography tests

---
+++ src/collaborative_platform/apps/nn_disambiguator/tests/unit_tests/test_geography.py
@@ -0,0 +1,15 @@
+from apps.nn_disambiguator.geography import *
+import pytest
+
+
+class TestGeography:
+    def test_coordinates(self):
+        assert coordinates("Salamanca, ES") == (40.9651572, -5.6640182)
+        assert coordinates("Warsaw, PL") == (52.2319581, 21.0067249)
+
+    def test_distance(self):
+        assert distance((0, 0), (0, 0)) == 0.
+        assert 7521 < distance((0, 0), (52, 52)) < 7522
+
+    def test_names_to_distance(self):
+        assert 2376 < names_to_distance("Salamanca, ES", "Warsaw, PL") < 2377
