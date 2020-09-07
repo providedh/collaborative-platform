8f664aba91467f9900badfc18cbc11507a9d1a08
janchorizo <jancho@usal.es>
2020-09-07 15:48:35 +0200

    Change operations utility to always retrieve a set object.

--- src/collaborative_platform/apps/close_reading/assets/src/common/helpers/operations.js
+++ src/collaborative_platform/apps/close_reading/assets/src/common/helpers/operations.js
@@ -3,15 +3,22 @@ export function discardOperations(operations, operationId) {
 	if (o === undefined) { raise('Tried to discard a non-existent operation') }
 
   const dependent = operations.filter(o => o.dependencies.includes(operationId))
-  if (dependent.length === 0) { return operationId }
-  return new Set([operationId, ...o.dependencies.map(discardOperations)])
+  if (dependent.length === 0) { return new Set([operationId]) }
+
+  const dependencies = dependent
+    .map(id => discardOperations(operations, id))
+    .reduce((ac, dc) => [...ac, ...dc], [])
+  return new Set([operationId, ...dependencies])
 }
 
 export function saveOperations(operations, operationId) {
 	const o = operations.filter(o => o.id === operationId)?.[0]
 	if (o === undefined) { raise('Tried to save a non-existent operation') }
 
-	if (o.dependencies.length === 0) { return operationId }
+	if (o.dependencies.length === 0) { return new Set([operationId]) }
 
-	return new Set([operationId, ...o.dependencies.map(saveOperations)])
+  const dependencies = o.dependencies
+    .map(id => saveOperations(operations, id))
+    .reduce((ac, dc) => [...ac, ...dc], [])
+	return new Set([operationId, ...dependencies])
 }
