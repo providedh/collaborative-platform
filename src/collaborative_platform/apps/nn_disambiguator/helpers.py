2571383371b3e494c9917c11940a92be2601c0a6
virdiignis <gbryk1@gmail.com>
2020-09-18 13:05:44 +0200

    prefit models (dummy way 4 now)

--- src/collaborative_platform/apps/nn_disambiguator/helpers.py
+++ src/collaborative_platform/apps/nn_disambiguator/helpers.py
@@ -2,16 +2,23 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 
 from apps.nn_disambiguator.models import Classifier
+from apps.nn_disambiguator.similarity_calculator import SimilarityCalculator
 from apps.projects.models import Project
 
 
 def create_models(project):  # type: (Project) -> None
     schemas = project.taxonomy.entities_schemas.all()
-    for entitiy in schemas:
+    for entity in schemas:
         model = MLPClassifier(hidden_layer_sizes=(15, 7, 2))
         scaler = StandardScaler()
 
-        dbo = Classifier(project=project, entity_schema=entitiy)
+        vector = [0] * SimilarityCalculator(entity).calculate_features_vector_length(entity)
+        scaler.partial_fit([vector])
+
+        model.partial_fit([vector], [0])
+        model.partial_fit([vector], [1])
+
+        dbo = Classifier(project=project, entity_schema=entity)
         dbo.save()
         dbo.set_model(model)
         dbo.set_scaler(scaler)
