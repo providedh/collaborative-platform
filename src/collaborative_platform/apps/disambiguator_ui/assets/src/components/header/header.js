14856401443cd311be7805091c25bb4e7a727523
janchorizo <jancho@usal.es>
2020-09-22 18:42:28 +0200

    Add bottom margin to header.

--- src/collaborative_platform/apps/disambiguator_ui/assets/src/components/header/header.js
+++ src/collaborative_platform/apps/disambiguator_ui/assets/src/components/header/header.js
@@ -6,7 +6,7 @@ import styles from './styles.module.css' // eslint-disable-line no-unused-vars
 
 export default function Header ({projectName, projectId, projectVersion, ...restProps}) {
   return (
-    <div className={styles.header}>
+    <div className={styles.header + ' mb-3'}>
       <h1>{projectName} <span>Current version: </span><span className="text-muted">v{projectVersion}</span></h1>
       <b>Disambiguating entities through unifications</b>
       <JobManager projectId={projectId}/>
