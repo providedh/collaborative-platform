c829d533f2a2ef1d2435c7eeedc4d9977f2a757e
janchorizo <jancho@usal.es>
2020-09-22 10:39:28 +0200

    Pass project version prop.

--- src/collaborative_platform/apps/disambiguator_ui/assets/src/components/app/app.js
+++ src/collaborative_platform/apps/disambiguator_ui/assets/src/components/app/app.js
@@ -5,13 +5,16 @@ import { Header } from 'components/header'
 import styles from './app.module.css' // eslint-disable-line no-unused-vars
 import defState from './def_state.js'
 
-export default function App (props) {
-  console.log(props)  
+export default function App ({projectName, projectId, projectVersion, ...restProps}) {
+  const appCssClasses = [
+    'container',
+    styles.app
+  ].join(' ')
+
   return (
-    <Header
-      projectName={props.projectName}
-      projectId={props.projectId}
-    />
+    <div className={appCssClasses}>
+      <Header {...{projectName, projectId, projectVersion}}/>
+    </div>
   )
 }
 
@@ -21,5 +24,6 @@ App.propTypes = {
   user: PropTypes.string,
   projectId: PropTypes.string,
   projectName: PropTypes.string,
+  projectVersion: PropTypes.string,
   configuration: PropTypes.object
 }
