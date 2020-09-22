db9de6385d33a78dde771f5f7badfaf8ab9e4ad3
janchorizo <jancho@usal.es>
2020-09-22 16:24:39 +0200

    Import the API in the job manager.

--- src/collaborative_platform/apps/disambiguator_ui/assets/src/components/jobManager/jobManager.js
+++ src/collaborative_platform/apps/disambiguator_ui/assets/src/components/jobManager/jobManager.js
@@ -1,10 +1,11 @@
 import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
+import {API} from 'common/helpers'
 import styles from './styles.module.css' // eslint-disable-line no-unused-vars
 
 export default function Jobs ({currentIndex, unifications, ...restProps}) {
-
+  window.API = API
   return (<div className={styles.jobs}></div>)
 }
 
