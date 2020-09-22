5434517ac1be0111d8cef7768688ca6b4ede98b8
janchorizo <jancho@usal.es>
2020-09-22 11:39:45 +0200

    Add content and style to unifications widget.

--- src/collaborative_platform/apps/disambiguator_ui/templatetags/unifications_widget.py
+++ src/collaborative_platform/apps/disambiguator_ui/templatetags/unifications_widget.py
@@ -4,5 +4,5 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('disambiguator_ui/unifications_widget.html')
-def unifications_widget():
-    return {}
+def unifications_widget(project_id):
+    return {"project_id": project_id}
