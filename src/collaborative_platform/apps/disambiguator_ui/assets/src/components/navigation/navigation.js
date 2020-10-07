a7bace511c2aece8e35ec0059468a9d85ad5ea24
janchorizo <jancho@usal.es>
2020-10-07 18:57:29 +0200

    Update navigation.

--- src/collaborative_platform/apps/disambiguator_ui/assets/src/components/navigation/navigation.js
+++ src/collaborative_platform/apps/disambiguator_ui/assets/src/components/navigation/navigation.js
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types'
 
 import styles from './styles.module.css' // eslint-disable-line no-unused-vars
 
-export default function Navigation ({proposals, listIndex, setListIndex, focusedIndex, setFocusedIndex}) {
+export default function Navigation ({proposals, listIndex, buffSize, ids, setListIndex, focusedIndex, setFocusedIndex}) {
   const [listShown, setListVisibility] = useState(false)
 
+  console.log(ids)
   const navigationCssClasses = [
     'd-flex',
     'position-relative',
@@ -24,13 +25,13 @@ export default function Navigation ({proposals, listIndex, setListIndex, focused
   return (<React.Fragment>
     <div className={navigationCssClasses}>
       <button
-          onClick={() => setIndex(Math.max(0, focusedIndex-1))}
+          onClick={() => setFocusedIndex(Math.max(0, focusedIndex-1))}
           type="button"
           className={navButtonCssClasses}>
         <span>⟵</span> <p className="d-inline m-0 p-0">Previous unification</p>
       </button>
       <div className={styles.listToggle}>
-        <b>{Math.min(focusedIndex + 1, proposals.length)} / {proposals.length}</b>
+        <b>{Math.min(focusedIndex + 1, proposals.length)} / {ids.length}</b>
         <button
           onClick={() => setListVisibility(!listShown)}
           type="button"
@@ -39,7 +40,7 @@ export default function Navigation ({proposals, listIndex, setListIndex, focused
       </button>
       </div>
       <button
-          onClick={() => setIndex(Math.min(proposals.length-1 , focusedIndex+1))}
+          onClick={() => setFocusedIndex(Math.min(proposals.length-1 , focusedIndex+1))}
           type="button"
           className={navButtonCssClasses}>
         <p className="d-inline m-0 p-0">Next unification</p> <span>⟶</span>
