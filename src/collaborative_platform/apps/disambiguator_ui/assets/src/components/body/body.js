b9c635b81f838e30e111e790d505dc01403ed528
janchorizo <jancho@usal.es>
2020-10-07 20:03:26 +0200

    Create target details component for the text description of an unification target.

--- src/collaborative_platform/apps/disambiguator_ui/assets/src/components/body/body.js
+++ src/collaborative_platform/apps/disambiguator_ui/assets/src/components/body/body.js
@@ -2,11 +2,14 @@ import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
 import styles from './styles.module.css' // eslint-disable-line no-unused-vars
+import { TargetDescription } from 'components/targetDescription'
 
-export default function Body ({focused}) {
+export default function Body ({projectId, focused}) {
   if (focused === null) {return ''}
   console.log(focused)
-  return (<div className={styles.body}></div>)
+  return (<div className={styles.body}>
+    <TargetDescription entity={focused.entity} projectId={projectId} />
+  </div>)
 }
 
 Body.propTypes = {
