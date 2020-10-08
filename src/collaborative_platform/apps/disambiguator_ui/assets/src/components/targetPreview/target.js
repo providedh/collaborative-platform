cac7ab3202cebcd8ea1990e7d8455873d7d2568f
janchorizo <jancho@usal.es>
2020-10-08 09:55:11 +0200

    Add entity highlighting.

--- src/collaborative_platform/apps/disambiguator_ui/assets/src/components/targetPreview/target.js
+++ src/collaborative_platform/apps/disambiguator_ui/assets/src/components/targetPreview/target.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 import styles from './styles.module.css' // eslint-disable-line no-unused-vars
 
-function useContent(container, url) {
+function useContent(container, url, entity) {
   useEffect(() => {
     fetch(url)
     .then(r => r.json())
@@ -13,10 +13,30 @@ function useContent(container, url) {
         const body =
           doc.getElementsByTagName('body')[0].innerHTML
         container.innerHTML = body
+        highlightTargets(container, entity)
       }
     })
     .catch(err => console.error(err))
   }, [url, container])
+}
+
+function useHighlight(container, entity) {
+  const target = `#${entity.type}-${entity.id}`
+  useEffect(() => {
+    highlightTargets(container, entity)
+  }, [target])
+}
+
+function highlightTargets(container, entity) {
+  const target = `#${entity.type}-${entity.id}`
+  if (container === undefined) {return}
+  Array.from(container.getElementsByTagName('name'))
+    .map(x => {
+      x.classList.remove(styles.highlighted)
+      return x
+    })
+    .filter(x => x.attributes?.ref?.value === target)
+    .forEach(x => x.classList.add(styles.highlighted))
 }
 
 export default function TargetPreview ({entity, projectId, configuration}) {
@@ -24,7 +44,8 @@ export default function TargetPreview ({entity, projectId, configuration}) {
   const preview = useRef()
 
   const fileURL = `/api/files/${entity.file_id}/`
-  useContent(preview.current, fileURL)
+  useContent(preview.current, fileURL, entity)
+  useHighlight(preview.current, entity)
 
   return (
     <div ref={preview} className={styles.targetPreview}>
