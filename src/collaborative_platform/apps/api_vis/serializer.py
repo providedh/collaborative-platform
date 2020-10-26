f64be0436edc7236925732a8b7026d5866d2e664
virdiignis <gbryk1@gmail.com>
2020-10-26 19:07:22 +0100

    add file id and name to entities endpoint

--- src/collaborative_platform/apps/api_vis/serializer.py
+++ src/collaborative_platform/apps/api_vis/serializer.py
@@ -17,7 +17,8 @@ class Serializer:
 
     @staticmethod
     def serialize_entity(entity):
-        serialized_entity = model_to_dict(entity, ['id', 'type'])
+        serialized_entity = model_to_dict(entity, ['id', 'type', 'xml_id', 'file'])
+        serialized_entity["file_name"] = entity.file.name
 
         return serialized_entity
 
