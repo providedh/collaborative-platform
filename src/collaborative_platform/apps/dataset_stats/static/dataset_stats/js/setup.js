0cdcbc4dccd290f60874edc621d82a188ca51b7c
janchorizo <jancho@usal.es>
2020-12-08 22:25:40 +0100

    Move dataset_stats inline setup code to static file.

---
+++ src/collaborative_platform/apps/dataset_stats/static/dataset_stats/js/setup.js
@@ -0,0 +1,16 @@
+(() => {
+    [...document.getElementById('app-params').children].forEach(param => {
+        const name = param.id;
+        window.p_raw = param.innerText
+        const value = name === 'preferences' ? JSON.parse(param.innerText.replace(/\\u0022/g, '"')) : param.innerText;
+        window[name] = value;
+    });
+    /*
+    var csrftoken = $("[name=csrfmiddlewaretoken]").val();
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    });
+    */
+})()
