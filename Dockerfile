245aef63ce67582b42320e26ee8a9b31bccf903a
virdiignis <gbryk1@gmail.com>
2021-02-17 17:37:51 +0100

    stop making /code

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM python:3
 ENV PYTHONUNBUFFERED 1
-RUN mkdir /code
+#RUN mkdir /code
 WORKDIR /code
 COPY requirements.txt /code/
 RUN apt update
 RUN apt install -y --no-install-recommends binutils libproj-dev gdal-bin
 RUN pip install -r requirements.txt
 RUN python -m spacy download en_core_web_lg
-COPY . /code/
+#COPY . /code/
 
 EXPOSE 8000
 STOPSIGNAL SIGINT
--- kubernetes/commands.sh
+++ kubernetes/commands.sh
@@ -3,5 +3,11 @@ mv *.yaml kubernetes
 oc login https://console.paas-dev.psnc.pl --token=
 oc apply -f redis-deployment.yaml
 docker login -u ti010 http://registry.apps.paas-dev.psnc.pl
-docker tag collaborative-platform_web registry.apps.paas-dev.psnc.pl/providedh-est/web:latest
-docker push registry.apps.paas-dev.psnc.pl/providedh-est/web/providedh-est/web:latest
+docker tag collaborative-platform_web registry.apps.paas-dev.psnc.pl/testing-providedh/web:latest
+docker push registry.apps.paas-dev.psnc.pl/testing-providedh/web/testing-providedh/web:latest
+oc new-app --docker-image="registry.apps.paas-dev.psnc.pl/testing-providedh/web:latest" https://github.com/providedh/collaborative-platform <źle
+oc new-app registry.apps.paas-dev.psnc.pl/testing-providedh/web:latest~https://github.com/providedh/collaborative-platform.git --strategy=docker
+
+hub.docker.com
+docker tag collaborative-platform_web ti010/collaborative_platform:latest                                                                                               [1]
+docker push ti010/collaborative_platform:latest
