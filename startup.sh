6c38ceac017fb23348e574a08054b258d9580c93
prance <gbryk1@gmail.com>
2021-03-21 11:47:19 +0100

    add uwsgi and daphne

--- startup.sh
+++ startup.sh
@@ -8,4 +8,9 @@ cd /app
 python src/collaborative_platform/manage.py makemigrations
 python src/collaborative_platform/manage.py migrate
 python src/collaborative_platform/manage.py loaddata core_initial.json
+rm -rf /app/static/*
+python src/collaborative_platform/manage.py collectstatic
 python src/collaborative_platform/manage.py runserver 0.0.0.0:8080
+cd src/collaborative_platform/
+daphne -u collaborative_platform_websockets.sock collaborative_platform.asgi:application &
+uwsgi uwsgi.ini
