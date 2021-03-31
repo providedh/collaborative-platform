76ccb55cec11f286b6411634257b1e1200b9efa9
prance <gbryk1@gmail.com>
2021-04-01 00:36:32 +0200

    test checkbox captcha

--- src/collaborative_platform/apps/core/forms.py
+++ src/collaborative_platform/apps/core/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from captcha.fields import ReCaptchaField
-from captcha.widgets import ReCaptchaV2Invisible
+from captcha.widgets import ReCaptchaV2Checkbox
 
 
 class SignUpForm(UserCreationForm):
